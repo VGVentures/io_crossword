ffb38b3bad4dc4db4258a2e8366e8bc77d060948
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-02-20 18:14:51 +0100

    feat: initialize firebase

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:io_crossword/app/app.dart';
 import 'package:io_crossword/bootstrap.dart';
 import 'package:io_crossword/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
