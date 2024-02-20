de435e462c3c5628a1ad718865a4cb7d7b21dff4
Jaime <52668514+jsgalarraga@users.noreply.github.com>
2024-02-20 19:59:20 +0100

    feat: add firebase (#1)
    * feat: add firebase
    
    * feat: initialize firebase
    
    * feat: remove firebase native config
    
    * feat: add web configuration

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,19 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:io_crossword/app/app.dart';
 import 'package:io_crossword/bootstrap.dart';
+import 'package:io_crossword/firebase_options.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  unawaited(
+    bootstrap(() => const App()),
+  );
 }
