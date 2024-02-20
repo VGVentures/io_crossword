7fc1b5e2d4f995ab108b8bbfe0efdc0ebeedd76a
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-02-20 18:04:50 +0100

    feat: add firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,16 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:io_crossword/app/app.dart';
 import 'package:io_crossword/bootstrap.dart';
+import 'package:io_crossword/firebase_options.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  unawaited(
+    bootstrap(() => const App()),
+  );
 }
