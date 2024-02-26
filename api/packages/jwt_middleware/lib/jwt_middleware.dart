85f29c2224a8bf14d89e3e2d560563b099a9264f
Hugo Walbecq <hugo@verygood.ventures>
2024-02-26 18:37:29 +0100

    feat: add jwt middleware (#7)
    * feat: add jwt middleware
    
    * chore: opti imports
    
    * chore: add package description

---
+++ api/packages/jwt_middleware/lib/jwt_middleware.dart
@@ -0,0 +1,6 @@
+/// A dart_frog middleware for checking JWT authorization.
+library jwt_middleware;
+
+export 'src/authenticated_user.dart';
+export 'src/jwt.dart';
+export 'src/jwt_middleware.dart';
