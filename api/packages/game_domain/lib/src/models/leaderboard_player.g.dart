10a4e6fc16be3e9589d055b13ee2bda812b4f89b
Hugo Walbecq <hugo@verygood.ventures>
2024-02-26 18:37:14 +0100

    feat: add game domain package (#4)
    * feat: add game domain package
    
    * feat: add workflow
    
    * chore: remove unused class and update descriptions

---
+++ api/packages/game_domain/lib/src/models/leaderboard_player.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'leaderboard_player.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LeaderboardPlayer _$LeaderboardPlayerFromJson(Map<String, dynamic> json) =>
+    LeaderboardPlayer(
+      id: json['id'] as String,
+      initials: json['initials'] as String,
+    );
+
+Map<String, dynamic> _$LeaderboardPlayerToJson(LeaderboardPlayer instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'initials': instance.initials,
+    };
---
+++ api/packages/game_domain/lib/src/models/models.dart
@@ -0,0 +1,1 @@
+export 'leaderboard_player.dart';
