54a8b2cf9c9b541400cba1f37c66186815bc8c24
Hugo Walbecq <hugo@verygood.ventures>
2024-02-27 15:36:10 +0100

    feat: add encryption middleware (#6)
    * feat: add encryption middleware
    
    * feat: add script to start local api
    
    * chore: package description
    
    * chore: remove script enabled api env variable

---
+++ scripts/start_local_api.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export FB_APP_ID=io-crossword-dev
+export GAME_URL=http://localhost:8080/
+export USE_EMULATOR=true
+export ENCRYPTION_KEY=X9YTchZdcnyZTNBSBgzj29p7RMBAIubD
+export ENCRYPTION_IV=FxC21ctRg9SgiXuZ
+export INITIALS_BLACKLIST_ID=MdOoZMhusnJTcwfYE0nL
+export FB_STORAGE_BUCKET=io-crossword-dev.appspot.com
+
+echo ' ######################## '
+echo ' ## Starting dart frog ## '
+echo ' ######################## '
+
+cd api && dart_frog dev
