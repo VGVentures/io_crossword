1c452d15603778659ec39208b91ef19b93438308
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-03-04 17:39:40 +0100

    feat: update board saving and logging

---
+++ packages/board_generator/lib/src/board_generator.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+
+import 'package:board_generator/src/models/data_model.dart';
+
+/// Generates a crossword.
+Crossword generateCrossword(List<String> wordList, String filePath) {
+  var crossword = Crossword((b) => b..candidates.addAll(wordList)).generate();
+
+  final before = DateTime.now();
+
+  final file = File(filePath);
+  final savedDownWords = <Location, String>{};
+  final savedAcrossWords = <Location, String>{};
+
+  while (crossword.candidates.isNotEmpty) {
+    crossword = crossword.generate();
+
+    // Save the words to a file. Only save every 100 words to avoid
+    // slowing down the process.
+    final wordCount = crossword.downWords.length + crossword.acrossWords.length;
+    if ((wordCount % 100 == 0) || (wordCount == wordList.length)) {
+      final now = DateTime.now();
+
+      print('Generated $wordCount words in ${now.difference(before)}');
+      print('Candidate locations: ${crossword.candidateLocations.length}');
+
+      final newAcrossWords = crossword.acrossWords.rebuild(
+        (b) =>
+            b..removeWhere((key, value) => savedAcrossWords.containsKey(key)),
+      );
+      final newDownWords = crossword.downWords.rebuild(
+        (b) => b..removeWhere((key, value) => savedDownWords.containsKey(key)),
+      );
+
+      final buffer = StringBuffer();
+      for (final entry in newAcrossWords.entries) {
+        buffer.writeln(
+          '${entry.key.across},${entry.key.down},${entry.value},horizontal',
+        );
+      }
+      for (final entry in newDownWords.entries) {
+        buffer.writeln(
+          '${entry.key.across},${entry.key.down},${entry.value},vertical',
+        );
+      }
+
+      file.writeAsStringSync(buffer.toString(), mode: FileMode.append);
+
+      savedAcrossWords.addAll(newAcrossWords.asMap());
+      savedDownWords.addAll(newDownWords.asMap());
+    }
+  }
+
+  return crossword;
+}
