8434fa8fb1ff2cd168b6347da8a5940bf3e73cf5
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-03-04 18:11:12 +0100

    feat: update board generation

---
+++ packages/board_generator/lib/generate_board.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:board_generator/src/board_generator.dart';
+
+void main(List<String> args) async {
+  final file = File('allWords.json');
+
+  final string = await file.readAsString();
+  final map = jsonDecode(string) as Map<String, dynamic>;
+  final words =
+      (map['words'] as List<dynamic>).map((e) => e as Map<String, dynamic>);
+  final parsedWords = words.map((word) {
+    return word['answer'] as String;
+  }).toList();
+
+  final regex = RegExp(r'^[a-z]+$');
+  final filteredWords = [...parsedWords]..removeWhere(
+      (element) => !regex.hasMatch(element),
+    );
+
+  generateCrossword(filteredWords, 'board.txt');
+}
