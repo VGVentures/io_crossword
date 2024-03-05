0dd3900ba086f7b6fc64ede4da163fcb393f4473
Jaime <52668514+jsgalarraga@users.noreply.github.com>
2024-03-05 14:59:53 +0100

    feat: improve board generator (#21)
    * feat: update word placement algo
    
    * feat: update board saving and logging
    
    * feat: save board to firestore
    
    * chore: add models package
    
    * feat: update board generation
    
    * feat: create and upload sections
    
    * docs: update readme
    
    * feat: improve readability
    
    * chore: update min coverage

---
+++ packages/board_generator/lib/src/crossword_repository.dart
@@ -0,0 +1,33 @@
+import 'package:dart_firebase_admin/firestore.dart';
+import 'package:game_domain/game_domain.dart';
+
+/// {@template crossword_repository}
+/// Repository for interacting with the crossword database in a dart only env.
+/// {@endtemplate}
+class CrosswordRepository {
+  /// {@macro crossword_repository}
+  CrosswordRepository({required this.firestore});
+
+  /// The firestore instance.
+  final Firestore firestore;
+
+  /// Adds a list of sections to the database.
+  Future<void> addSections(List<BoardSection> sections) async {
+    for (final section in sections) {
+      await firestore.collection('boardSections').add(section.toJson());
+    }
+  }
+
+  /// Deletes all sections from the database.
+  Future<void> deleteSections() async {
+    var docs = await firestore.collection('boardSections').listDocuments();
+    var length = docs.length;
+    while (length > 0) {
+      for (final doc in docs) {
+        await doc.delete();
+      }
+      docs = await firestore.collection('boardSections').listDocuments();
+      length = docs.length;
+    }
+  }
+}
