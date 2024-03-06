0e3e2e0cb2cae6c9533f26e94f5d0fbaef55e9ed
AyadLaouissi <ayadlaouissi@gmail.com>
2024-03-06 15:51:18 +0100

    adds logic to leaderboard_repository.dart

--- api/packages/game_domain/lib/src/models/leaderboard_player.g.dart
+++ api/packages/game_domain/lib/src/models/leaderboard_player.g.dart
@@ -10,10 +10,12 @@ LeaderboardPlayer _$LeaderboardPlayerFromJson(Map<String, dynamic> json) =>
     LeaderboardPlayer(
       id: json['id'] as String,
       initials: json['initials'] as String,
+      numSolved: json['numSolved'] as int,
     );
 
 Map<String, dynamic> _$LeaderboardPlayerToJson(LeaderboardPlayer instance) =>
     <String, dynamic>{
       'id': instance.id,
+      'numSolved': instance.numSolved,
       'initials': instance.initials,
     };
--- api/packages/leaderboard_repository/test/src/leaderboard_repository_test.dart
+++ api/packages/leaderboard_repository/test/src/leaderboard_repository_test.dart
@@ -1,11 +1,133 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:db_client/db_client.dart';
+import 'package:game_domain/game_domain.dart';
 import 'package:leaderboard_repository/leaderboard_repository.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+
+class _MockDbClient extends Mock implements DbClient {}
 
 void main() {
   group('LeaderboardRepository', () {
+    late DbClient dbClient;
+    late LeaderboardRepository leaderboardRepository;
+
+    const blacklistDocumentId = 'id';
+
+    setUp(() {
+      dbClient = _MockDbClient();
+      leaderboardRepository = LeaderboardRepository(
+        dbClient: dbClient,
+        blacklistDocumentId: blacklistDocumentId,
+      );
+    });
+
     test('can be instantiated', () {
-      expect(LeaderboardRepository(), isNotNull);
+      expect(
+        LeaderboardRepository(
+          dbClient: dbClient,
+          blacklistDocumentId: blacklistDocumentId,
+        ),
+        isNotNull,
+      );
+    });
+
+    group('getLeaderboard', () {
+      test('returns list of leaderboard players', () async {
+        const playerOne = LeaderboardPlayer(
+          id: 'id',
+          initials: 'AAA',
+          numSolved: 20,
+        );
+        const playerTwo = LeaderboardPlayer(
+          id: 'id2',
+          initials: 'BBB',
+          numSolved: 10,
+        );
+
+        when(() => dbClient.orderBy('leaderboard', 'numSolved'))
+            .thenAnswer((_) async {
+          return [
+            DbEntityRecord(
+              id: 'id',
+              data: {
+                'initials': playerOne.initials,
+                'numSolved': 20,
+              },
+            ),
+            DbEntityRecord(
+              id: 'id2',
+              data: {
+                'initials': playerTwo.initials,
+                'numSolved': 10,
+              },
+            ),
+          ];
+        });
+
+        final result = await leaderboardRepository.getLeaderboard();
+
+        expect(result, equals([playerOne, playerTwo]));
+      });
+
+      test('returns empty list if results are empty', () async {
+        when(() => dbClient.orderBy('leaderboard', 'numSolved'))
+            .thenAnswer((_) async {
+          return [];
+        });
+
+        final response = await leaderboardRepository.getLeaderboard();
+        expect(response, isEmpty);
+      });
+    });
+
+    group('addPlayerToLeaderboard', () {
+      test('returns empty list if results are empty', () async {
+        final leaderboardPlayer = LeaderboardPlayer(
+          id: 'id',
+          initials: 'initials',
+          numSolved: 40,
+        );
+
+        when(() => dbClient.add('leaderboard', leaderboardPlayer.toJson()))
+            .thenAnswer((_) async {
+          return '12345678';
+        });
+
+        final response = await leaderboardRepository.addPlayerToLeaderboard(
+          leaderboardPlayer: leaderboardPlayer,
+        );
+
+        expect(response, equals('12345678'));
+      });
+    });
+
+    group('getInitialsBlacklist', () {
+      const blacklist = ['AAA', 'BBB', 'CCC'];
+
+      test('returns the blacklist', () async {
+        when(() => dbClient.getById('initials_blacklist', blacklistDocumentId))
+            .thenAnswer(
+          (_) async => DbEntityRecord(
+            id: blacklistDocumentId,
+            data: const {
+              'blacklist': ['AAA', 'BBB', 'CCC'],
+            },
+          ),
+        );
+
+        final response = await leaderboardRepository.getInitialsBlacklist();
+        expect(response, equals(blacklist));
+      });
+
+      test('returns empty list if not found', () async {
+        when(() => dbClient.getById('initials_blacklist', any())).thenAnswer(
+          (_) async => null,
+        );
+
+        final response = await leaderboardRepository.getInitialsBlacklist();
+        expect(response, isEmpty);
+      });
     });
   });
 }
