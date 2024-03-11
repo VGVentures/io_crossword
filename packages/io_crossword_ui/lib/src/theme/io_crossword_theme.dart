8ee50dadf57e83e6dfac6b8cf60955925da2a681
Hugo Walbecq <hugo@verygood.ventures>
2024-03-11 16:11:28 +0100

    feat: UI package (#53)
    * feat: create ui package
    
    * feat: update theme
    
    * feat: add fonts
    
    * feat: workflow
    
    * chore: update pubspec
    
    * test: add theme tests
    
    * chore: rename test file
    
    * chore: missing coma...

---
+++ packages/io_crossword_ui/lib/src/theme/io_crossword_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:io_crossword_ui/io_crossword_ui.dart';
+
+/// {@template io_crossword_theme}
+/// IO Crossword theme.
+/// {@endtemplate}
+class IoCrosswordTheme {
+  /// [ThemeData] for IO Crossword.
+  static ThemeData get themeData {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: _colorScheme,
+      textTheme: _textTheme.apply(
+        bodyColor: IoCrosswordColors.seedWhite,
+        displayColor: IoCrosswordColors.seedWhite,
+        decorationColor: IoCrosswordColors.seedWhite,
+      ),
+    );
+  }
+
+  static ColorScheme get _colorScheme {
+    return ColorScheme.fromSeed(
+      seedColor: IoCrosswordColors.seedBlue,
+      background: IoCrosswordColors.seedBlack,
+    );
+  }
+
+  static TextTheme get _textTheme {
+    final isMobile = defaultTargetPlatform == TargetPlatform.android ||
+        defaultTargetPlatform == TargetPlatform.iOS;
+
+    return isMobile
+        ? IoCrosswordTextStyles.mobile.textTheme
+        : IoCrosswordTextStyles.desktop.textTheme;
+  }
+}
