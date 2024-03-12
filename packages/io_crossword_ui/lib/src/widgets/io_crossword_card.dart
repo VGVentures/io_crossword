e4ba70ec213e0a2918b0ba24dcea3eed52e18d5e
Alejandro Santiago <dev@alestiago.com>
2024-03-12 11:36:41 +0000

    feat: removed orientation changes

--- packages/io_crossword_ui/lib/src/widgets/io_crossword_card.dart
+++ packages/io_crossword_ui/lib/src/widgets/io_crossword_card.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:io_crossword_ui/io_crossword_ui.dart';
 
 /// {@template io_crossword_card}
 /// A panel with slightly rounded corners and an elevation shadow.
 ///
 /// It has a fixed maximum size that adjusts according to the device's
-/// orientation.
+/// layout size.
+///
+/// Does not support orientation changes, it assumes the orientation is fixed
+/// to portrait.
 /// {@endtemplate}
 class IoCrosswordCard extends StatelessWidget {
   /// {@macro io_crossword_card}
@@ -20,24 +24,13 @@ class IoCrosswordCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OrientationBuilder(
-      builder: (context, orientation) {
-        final constraints = switch (orientation) {
-          Orientation.portrait =>
-            const BoxConstraints(maxWidth: 358, maxHeight: 540),
-          Orientation.landscape =>
-            const BoxConstraints(maxWidth: 540, maxHeight: 358),
-        };
-
-        return ConstrainedBox(
-          constraints: constraints,
-          child: Material(
-            type: MaterialType.card,
-            borderRadius: BorderRadius.circular(24),
-            child: SizedBox.expand(child: child),
-          ),
-        );
-      },
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 358, maxHeight: 540),
+      child: Material(
+        type: MaterialType.card,
+        borderRadius: BorderRadius.circular(24),
+        child: SizedBox.expand(child: child),
+      ),
     );
   }
 }
