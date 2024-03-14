558772ae04bc5450cd9e966cb3d0a3bed70a2e9a
AyadLaouissi <ayadlaouissi@gmail.com>
2024-03-14 11:41:46 +0100

    feat: adds sort words for easy and efficient finding for board position

---
+++ packages/board_generator/lib/src/sort_words.dart
@@ -0,0 +1,81 @@
+/// Generates the words organised by the length of the word + character
+/// position + the character.
+///
+/// This way the searches to find a word for the available space is performed
+/// with good performance.
+///
+/// Example:
+/// The list of words ['Aunt', 'Addy', 'Adds', 'Away']
+///
+/// would be organized as
+///
+/// final organizedWords = <int, Map<int, Map<String, Set<String>>>>{
+///   4: {
+///     0: {
+///       'a': {'Adds', 'Addy', 'Aunt', 'Away'},
+///     },
+///     1: {
+///       'u': {'Aunt'},
+///       'd': {'Adds', 'Addy'},
+///       'w': {'Away'},
+///     },
+///     2: {
+///       'n': {'Aunt'},
+///       'd': {'Adds', 'Addy'},
+///       'a': {'Away'},
+///     },
+///     3: {
+///       't': {'Aunt'},
+///       'y': {'Addy', 'Away'},
+///       's': {'Adds'},
+///     },
+///   },
+/// };
+///
+/// We can perform the following to search for words that can be added
+/// in the crossword.
+///
+/// final fourCharacters = organizedWords[4];
+///
+///  If the only requirement is to have the character starting with a
+///  we can just get the first
+/// organizedWords[4][0]['a'].first; // Aunt
+///
+/// organizedWords[4][0]['a']; // Aunt, Addy, Adds, Away
+/// organizedWords[4][1]['d']; // 'Addy' 'Adds'
+/// organizedWords[4][1]['u']; // Aunt
+///
+/// If we need a word starts with a and in the second position also with a
+/// organizedWords[4][0]['a'].firstWhere((word) => word.startsWith('a', 2)); // Away
+///
+/// If we need a word starts with a, the second position with d and ends with s
+/// organizedWords[4][0]['a'].firstWhere((word) => word.startsWith('d', 2) && word.endsWith('s')); // Adds
+///
+/// Looks like: Map<int(numberOfCharacters), Map<int(characterPosition,
+/// Map<String(letterCharacter), Set<String(Word)>>>>
+Map<int, Map<int, Map<String, Set<String>>>> sortWords(
+  List<String> wordList,
+) {
+  final map = <int, Map<int, Map<String, Set<String>>>>{};
+
+  for (final word in wordList) {
+    if (!map.containsKey(word.length)) {
+      map[word.length] = <int, Map<String, Set<String>>>{
+        for (var i = 0; i < word.length; i++) i: <String, Set<String>>{},
+      };
+    }
+
+    for (var i = 0; i < word.length; i++) {
+      final character = word[i].toLowerCase();
+      final mapCharacterPosition = map[word.length]![i]!;
+
+      if (!mapCharacterPosition.containsKey(character)) {
+        mapCharacterPosition[character] = <String>{word};
+      }
+
+      mapCharacterPosition[character]!.add(word);
+    }
+  }
+
+  return map;
+}
