20772d384a2d8e5c32a90506ca8fe57782e209a1
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-03-18 10:35:51 +0100

    feat: add board info repository

---
+++ packages/board_info_repository/lib/src/board_info_repository.dart
@@ -0,0 +1,7 @@
+/// {@template board_info_repository}
+/// A repository to manage the general board information and parameters
+/// {@endtemplate}
+class BoardInfoRepository {
+  /// {@macro board_info_repository}
+  const BoardInfoRepository();
+}
