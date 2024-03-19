2fecc3995b9bf6aaa0d0a224f6aacfa0d37a5a0c
Alejandro Santiago <dev@alestiago.com>
2024-03-19 18:33:02 +0000

    feat: defined word_entry surroundings

--- packages/board_generator_playground/lib/src/models/word_entry.dart
+++ packages/board_generator_playground/lib/src/models/word_entry.dart
@@ -21,6 +21,62 @@ class WordEntry extends Equatable {
   /// {@macro direction}
   final Direction direction;
 
+  /// The surrounding locations of the word entry.
+  ///
+  /// For example:
+  ///
+  /// Consider the following board
+  ///
+  /// ```
+  ///    -2 -1  0  1  2
+  /// -3  -  -  -  -  -
+  /// -2  -  -  B  U  S
+  /// -1  -  -  E  -  -
+  ///  0  -  -  H  -  -
+  ///  1  -  -  A  -  -
+  ///  2  -  -  N  -  -
+  ///  3  -  -  -  -  -
+  /// ```
+  ///
+  /// The surrounding locations of the word "BUSH" are:
+  /// (0, -3), (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (0, 3), (1, 2),
+  /// (1, 1), (1, 0), (1, -1) and (1, -2).
+  ///
+  /// If we label those surrounding locations with "*" we get:
+  ///
+  ///
+  /// ```
+  ///    -2 -1  0  1  2
+  /// -3  -  -  *  -  -
+  /// -2  -  *  B  *  S
+  /// -1  -  *  E  *  -
+  ///  0  -  *  H  *  -
+  ///  1  -  *  A  *  -
+  ///  2  -  *  N  *  -
+  ///  3  -  -  *  -  -
+  /// ```
+  Set<Location> surroundings() {
+    final prefix = direction == Direction.across
+        ? location.copyWith(x: location.x - 1)
+        : location.copyWith(y: location.y - 1);
+    final suffix = direction == Direction.across
+        ? location.copyWith(x: location.x + word.length)
+        : location.copyWith(y: location.y + word.length);
+
+    return <Location>{
+      prefix,
+      suffix,
+      for (var i = 0; i < word.length; i++)
+        direction == Direction.across
+            ? Location(x: location.x + i, y: location.y - 1)
+            : Location(x: location.x - 1, y: location.y + i),
+      for (var i = 0; i < word.length; i++)
+        direction == Direction.across
+            ? Location(x: location.x + i, y: location.y + 1)
+            : Location(x: location.x + 1, y: location.y + i),
+    };
+  }
+
   @override
   List<Object?> get props => [word, location, direction];
 }
