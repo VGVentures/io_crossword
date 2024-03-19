9085fa9665f6648e4c8b3d54d52a0dd004035862
Hugo Walbecq <hugo@verygood.ventures>
2024-03-19 12:24:50 +0100

    test: add missing test on snapshot render mode

--- test/crossword/game/components/section_component_test.dart
+++ test/crossword/game/components/section_component_test.dart
@@ -250,6 +250,69 @@ void main() {
     );
 
     testWithGame(
+      'loads the section in snapshot mode when the state changes and the '
+      'section is present',
+      createGame,
+      (game) async {
+        final streamController = StreamController<CrosswordState>.broadcast();
+        when(() => bloc.stream).thenAnswer((_) => streamController.stream);
+        when(() => bloc.state).thenReturn(
+          const CrosswordLoaded(
+            sectionSize: 400,
+            sections: {},
+          ),
+        );
+        final component = SectionComponent(index: (100, 100));
+        await game.world.ensureAdd(component);
+
+        var spriteComponent = component.firstChild<SpriteComponent>();
+
+        expect(spriteComponent, isNull);
+        streamController.add(
+          CrosswordLoaded(
+            sectionSize: 400,
+            renderMode: RenderMode.snapshot,
+            sections: {
+              (100, 100): BoardSection(
+                id: '',
+                position: const Point(100, 100),
+                size: 400,
+                snapshotUrl: 'snapshotUrl',
+                words: [
+                  Word(
+                    position: const Point(0, 0),
+                    axis: Axis.vertical,
+                    answer: 'Flutter',
+                    clue: '',
+                    hints: const [],
+                    solvedTimestamp: null,
+                  ),
+                  Word(
+                    position: const Point(0, 0),
+                    axis: Axis.horizontal,
+                    answer: 'Firebase',
+                    clue: '',
+                    hints: const [],
+                    solvedTimestamp: null,
+                  ),
+                ],
+                borderWords: const [],
+              ),
+            },
+          ),
+        );
+
+        await game.ready();
+
+        spriteComponent = component.firstChild<SpriteComponent>();
+        expect(spriteComponent, isNotNull);
+
+        final sprite = spriteComponent?.sprite;
+        expect(sprite, isNotNull);
+      },
+    );
+
+    testWithGame(
       'adds a BoardSectionRequested when the section is not loaded yet',
       createGame,
       (game) async {
@@ -264,7 +327,7 @@ void main() {
     );
 
     testWithGame(
-      'switches rendering to snapshot when render mode changes',
+      'switches rendering from game to snapshot',
       createGame,
       (game) async {
         final streamController = StreamController<CrosswordState>.broadcast();
@@ -325,6 +388,70 @@ void main() {
 
         final spriteComponent = sectionComponent.firstChild<SpriteComponent>();
         expect(spriteComponent, isNotNull);
+      },
+    );
+
+    testWithGame(
+      'switches rendering from snapshot to game',
+      createGame,
+      (game) async {
+        final streamController = StreamController<CrosswordState>.broadcast();
+        final state = CrosswordLoaded(
+          sectionSize: 400,
+          renderMode: RenderMode.snapshot,
+          sections: {
+            (0, 0): BoardSection(
+              id: '',
+              position: const Point(0, 0),
+              size: 400,
+              words: [
+                Word(
+                  position: const Point(1, 0),
+                  axis: Axis.vertical,
+                  answer: 'Flutter',
+                  clue: '',
+                  hints: const [],
+                  solvedTimestamp: null,
+                ),
+                Word(
+                  position: const Point(0, 0),
+                  axis: Axis.horizontal,
+                  answer: 'Firebase',
+                  clue: '',
+                  hints: const [],
+                  solvedTimestamp: null,
+                ),
+              ],
+              snapshotUrl: 'snapshotUrl',
+              borderWords: const [],
+            ),
+          },
+        );
+        when(() => bloc.state).thenReturn(state);
+        whenListen(bloc, streamController.stream, initialState: state);
+        await game.ready();
+
+        streamController.add(
+          state.copyWith(
+            renderMode: RenderMode.game,
+          ),
+        );
+
+        await Future.microtask(() {});
+        await Future.microtask(() {});
+        game.update(0);
+
+        final sectionComponent = game.world.children
+            .whereType<SectionComponent>()
+            .firstWhere((element) => element.index == (0, 0));
+        expect(sectionComponent, isNotNull);
+
+        final spriteBatchComponent =
+            sectionComponent.firstChild<SpriteBatchComponent>();
+        expect(spriteBatchComponent, isNotNull);
+
+        final spriteComponent = sectionComponent.firstChild<SpriteComponent>();
+        expect(spriteComponent, isNull);
       },
     );
   });
