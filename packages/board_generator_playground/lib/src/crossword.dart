99193153fcfc6baa95476260f1f938f0dc5b3fd3
Alejandro Santiago <dev@alestiago.com>
2024-03-19 10:54:46 +0000

    feat: define methods

--- packages/board_generator_playground/lib/src/crossword.dart
+++ packages/board_generator_playground/lib/src/crossword.dart
@@ -31,4 +31,112 @@ class Crossword {
       characterMap[newLocation] = character;
     }
   }
+
+  /// Determines if a new [entry] will be connected to the existing board.
+  ///
+  /// For example:
+  ///
+  /// Considering this board:
+  ///
+  /// ```
+  /// - - - A L B U S
+  /// - - - - - E - -
+  /// - - - - - H - -
+  /// - - - - - A - -
+  /// - - - - - N - -
+  /// ```
+  ///
+  /// Adding the word "SUN" down at the end of the word "ALBUS" would be
+  /// connected:
+  ///
+  /// ```
+  /// - - - A L B U S
+  /// - - - - - E - U
+  /// - - - - - H - N
+  /// - - - - - A - -
+  /// - - - - - N - -
+  /// ```
+  ///
+  /// However, adding the word "SUN" at the bottom left corner would not be
+  /// connected:
+  ///
+  /// ```
+  /// - - - A L B U S
+  /// - - - - - E - -
+  /// S - - - - H - -
+  /// U - - - - A - -
+  /// N - - - - N - -
+  /// ```
+  bool isConnected(WordEntry entry) {
+    throw UnimplementedError();
+  }
+
+  /// Determines all the connections for a new [entry].
+  ///
+  /// For example:
+  ///
+  /// Considering this board:
+  ///
+  /// ```
+  /// - - - A L B U S
+  /// - - - - - E - -
+  /// - - - - - H - -
+  /// - - - - - A - -
+  /// - - - - - N - -
+  /// ```
+  ///
+  /// Adding the word "USA down the "U" in "ALBUS" would have a total of
+  /// 4 connections:
+  ///
+  /// ```
+  /// - - - A L B U S
+  /// - - - - - E S -
+  /// - - - - - H A -
+  /// - - - - - A - -
+  /// - - - - - N - -
+  /// ```
+  Set<Location> connections(WordEntry entry) {
+    throw UnimplementedError();
+  }
+
+  /// Whether the new [entry] overlaps an existing word.
+  ///
+  /// For example:
+  ///
+  /// Considering this board:
+  ///
+  /// ```
+  /// - - - A L B U S
+  /// - - - - - E - -
+  /// - - - - - H - -
+  /// - - - - - A - -
+  /// - - - - - N - -
+  /// ```
+  ///
+  /// Adding the word "USA" across to the right of the word "ALBUS" would
+  /// overlap:
+  ///
+  /// ```
+  /// U S A A L B U S
+  /// - - - - - E - -
+  /// - - - - - H - -
+  /// - - - - - A - -
+  /// - - - - - N - -
+  /// ```
+  ///
+  /// However, adding the word "SUN" down at the end of the word "ALBUS" would
+  /// not overlap:
+  ///
+  /// ```
+  /// - - - A L B U S
+  /// - - - - - E - U
+  /// - - - - - H - N
+  /// - - - - - A - -
+  /// - - - - - N - -
+  /// ```
+  ///
+  /// Overlaps are not allowed since they would create invalid words.
+  bool overlaps(WordEntry entry) {
+    throw UnimplementedError();
+  }
 }
