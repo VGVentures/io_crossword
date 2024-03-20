bfc4b2bbff56b8bfb1d3ee688d8fd419d331a0e6
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-03-20 18:49:03 +0100

    chore: add local launch config

--- scripts/start_local_api.sh
+++ scripts/start_local_api.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export FB_APP_ID=io-crossword-dev
-export GAME_URL=http://localhost:8080/
-export USE_EMULATOR=true
+export GAME_URL=http://localhost:24514
+export USE_EMULATOR=false
 export ENCRYPTION_KEY=X9YTchZdcnyZTNBSBgzj29p7RMBAIubD
 export ENCRYPTION_IV=FxC21ctRg9SgiXuZ
 export INITIALS_BLACKLIST_ID=T1ilfCwjDpLS7iaFzenA
