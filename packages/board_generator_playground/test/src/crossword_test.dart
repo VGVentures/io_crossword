e68ca73761e88a55a87d112ec0940902db2094f2
Alejandro Santiago <dev@alestiago.com>
2024-03-20 10:46:47 +0000

    feat(game_generator_playground): include Crossword methods (#97)
    Co-authored-by: AyadLaouissi <ayadlaouissi@gmail.com>

--- packages/board_generator_playground/test/src/crossword_test.dart
+++ packages/board_generator_playground/test/src/crossword_test.dart
@@ -4,25 +4,36 @@ import 'package:board_generator_playground/src/crossword.dart';
 import 'package:board_generator_playground/src/models/models.dart';
 import 'package:test/test.dart';
 
+import 'fixtures/crosswords.dart';
+
 void main() {
   group('$Crossword', () {
     group('add', () {
       test('adds a down word to the character map', () {
         final board = Crossword();
 
-        const urlWordEntry = WordEntry(
+        final url = WordEntry(
           word: 'url',
-          location: Location(x: 0, y: -1),
+          start: Location(x: 0, y: -1),
           direction: Direction.down,
         );
-        board.add(urlWordEntry);
+        board.add(url);
 
         expect(
           board.characterMap,
           {
-            Location(x: 0, y: -1): 'u',
-            Location(x: 0, y: 0): 'r',
-            Location(x: 0, y: 1): 'l',
+            Location(x: 0, y: -1): CharacterData(
+              character: 'u',
+              wordEntry: {url},
+            ),
+            Location(x: 0, y: 0): CharacterData(
+              character: 'r',
+              wordEntry: {url},
+            ),
+            Location(x: 0, y: 1): CharacterData(
+              character: 'l',
+              wordEntry: {url},
+            ),
           },
         );
       });
@@ -30,20 +41,431 @@ void main() {
       test('adds an across word to the character map', () {
         final board = Crossword();
 
-        const urlWordEntry = WordEntry(
+        final url = WordEntry(
           word: 'url',
-          location: Location(x: -1, y: 0),
+          start: Location(x: -1, y: 0),
           direction: Direction.across,
         );
-        board.add(urlWordEntry);
+        board.add(url);
 
         expect(
           board.characterMap,
           {
-            Location(x: -1, y: 0): 'u',
-            Location(x: 0, y: 0): 'r',
-            Location(x: 1, y: 0): 'l',
+            Location(x: -1, y: 0): CharacterData(
+              character: 'u',
+              wordEntry: {url},
+            ),
+            Location(x: 0, y: 0): CharacterData(
+              character: 'r',
+              wordEntry: {url},
+            ),
+            Location(x: 1, y: 0): CharacterData(
+              character: 'l',
+              wordEntry: {url},
+            ),
           },
+        );
+      });
+
+      test('adds crossing words', () {
+        final behan = WordEntry(
+          word: 'behan',
+          start: Location(x: 0, y: -2),
+          direction: Direction.down,
+        );
+        final albus = WordEntry(
+          word: 'albus',
+          start: Location(x: -2, y: -2),
+          direction: Direction.across,
+        );
+
+        final board = Crossword()
+          ..add(behan)
+          ..add(albus);
+
+        expect(
+          board.characterMap,
+          equals(
+            {
+              Location(x: 0, y: -2): CharacterData(
+                character: 'b',
+                wordEntry: {
+                  WordEntry(
+                    word: 'behan',
+                    start: Location(x: 0, y: -2),
+                    direction: Direction.down,
+                  ),
+                  WordEntry(
+                    word: 'albus',
+                    start: Location(x: -2, y: -2),
+                    direction: Direction.across,
+                  ),
+                },
+              ),
+              Location(x: 0, y: -1): CharacterData(
+                character: 'e',
+                wordEntry: {
+                  WordEntry(
+                    word: 'behan',
+                    start: Location(x: 0, y: -2),
+                    direction: Direction.down,
+                  ),
+                },
+              ),
+              Location(x: 0, y: 0): CharacterData(
+                character: 'h',
+                wordEntry: {
+                  WordEntry(
+                    word: 'behan',
+                    start: Location(x: 0, y: -2),
+                    direction: Direction.down,
+                  ),
+                },
+              ),
+              Location(x: 0, y: 1): CharacterData(
+                character: 'a',
+                wordEntry: {
+                  WordEntry(
+                    word: 'behan',
+                    start: Location(x: 0, y: -2),
+                    direction: Direction.down,
+                  ),
+                },
+              ),
+              Location(x: 0, y: 2): CharacterData(
+                character: 'n',
+                wordEntry: {
+                  WordEntry(
+                    word: 'behan',
+                    start: Location(x: 0, y: -2),
+                    direction: Direction.down,
+                  ),
+                },
+              ),
+              Location(x: -2, y: -2): CharacterData(
+                character: 'a',
+                wordEntry: {
+                  WordEntry(
+                    word: 'albus',
+                    start: Location(x: -2, y: -2),
+                    direction: Direction.across,
+                  ),
+                },
+              ),
+              Location(x: -1, y: -2): CharacterData(
+                character: 'l',
+                wordEntry: {
+                  WordEntry(
+                    word: 'albus',
+                    start: Location(x: -2, y: -2),
+                    direction: Direction.across,
+                  ),
+                },
+              ),
+              Location(x: 1, y: -2): CharacterData(
+                character: 'u',
+                wordEntry: {
+                  WordEntry(
+                    word: 'albus',
+                    start: Location(x: -2, y: -2),
+                    direction: Direction.across,
+                  ),
+                },
+              ),
+              Location(x: 2, y: -2): CharacterData(
+                character: 's',
+                wordEntry: {
+                  WordEntry(
+                    word: 'albus',
+                    start: Location(x: -2, y: -2),
+                    direction: Direction.across,
+                  ),
+                },
+              ),
+            },
+          ),
+        );
+      });
+    });
+
+    group('isConnected', () {
+      test('connected to the board in the position (2, -2) with s', () {
+        final board = Crossword1();
+
+        final sun = WordEntry(
+          word: 'sun',
+          start: Location(x: 2, y: -2),
+          direction: Direction.down,
+        );
+
+        expect(board.isConnected(sun), isTrue);
+      });
+
+      test('not connected to the board in the position (2, -1) with s', () {
+        final board = Crossword1();
+
+        final sun = WordEntry(
+          word: 'sun',
+          start: Location(x: 2, y: -1),
+          direction: Direction.down,
+        );
+
+        expect(board.isConnected(sun), isFalse);
+      });
+
+      test('connected to the board in the position (0, 0) with h', () {
+        final board = Crossword1();
+
+        final hat = WordEntry(
+          word: 'hat',
+          start: Location(x: 0, y: 0),
+          direction: Direction.across,
+        );
+
+        expect(board.isConnected(hat), isTrue);
+      });
+    });
+
+    group('overrides', () {
+      test('returns false when complete empty spot', () {
+        final board = Crossword1();
+
+        final word = WordEntry(
+          word: 'egg',
+          start: Location(x: 1, y: -1),
+          direction: Direction.across,
+        );
+
+        expect(board.overrides(word), isFalse);
+      });
+    });
+
+    group('overlaps', () {
+      test('returns true when complete empty spot', () {
+        final board = Crossword3();
+
+        final word = WordEntry(
+          word: 'sandy',
+          start: Location(x: 2, y: -2),
+          direction: Direction.down,
+        );
+
+        expect(board.overlaps(word), isTrue);
+      });
+
+      test('returns true when overlaps horizontal', () {
+        final board = Crossword4();
+
+        final word = WordEntry(
+          word: 'sand',
+          start: Location(x: 2, y: -2),
+          direction: Direction.down,
+        );
+
+        expect(board.overlaps(word), isTrue);
+      });
+
+      test('returns true when complete empty spot', () {
+        final board = Crossword3();
+
+        final word = WordEntry(
+          word: 'sandy',
+          start: Location(x: 2, y: -1),
+          direction: Direction.down,
+        );
+
+        expect(board.overlaps(word), isTrue);
+      });
+    });
+
+    group('connections', () {
+      test('gets 4 connections for usa down at (1, -2)', () {
+        final board = Crossword1();
+
+        final usa = WordEntry(
+          word: 'usa',
+          start: Location(x: 1, y: -2),
+          direction: Direction.down,
+        );
+
+        expect(
+          board.connections(usa),
+          <Location>{
+            Location(x: 2, y: -2),
+            Location(x: 0, y: -2),
+            Location(x: 0, y: -1),
+            Location(x: 0, y: 0),
+          },
+        );
+      });
+
+      test('gets 1 connections for usa down at (2, -2)', () {
+        final board = Crossword1();
+
+        final usa = WordEntry(
+          word: 'sand',
+          start: Location(x: 2, y: -2),
+          direction: Direction.down,
+        );
+
+        expect(
+          board.connections(usa),
+          <Location>{
+            Location(x: 1, y: -2),
+          },
+        );
+      });
+
+      test('gets 3 connections for usa down at (2, -2)', () {
+        final board = Crossword1();
+
+        final usa = WordEntry(
+          word: 'sa',
+          start: Location(x: 1, y: -1),
+          direction: Direction.down,
+        );
+
+        expect(
+          board.connections(usa),
+          <Location>{
+            Location(x: 1, y: -2),
+            Location(x: 0, y: -1),
+            Location(x: 0, y: 0),
+          },
+        );
+      });
+
+      test('gets 2 connections for across down at (-1, 1)', () {
+        final board = Crossword1();
+
+        final usa = WordEntry(
+          word: 'usa',
+          start: Location(x: -2, y: 1),
+          direction: Direction.across,
+        );
+
+        expect(
+          board.connections(usa),
+          <Location>{
+            Location(x: 0, y: 0),
+            Location(x: 0, y: 2),
+          },
+        );
+      });
+
+      test('gets 1 connections for sand across at (2, -2)', () {
+        final board = Crossword1();
+
+        final usa = WordEntry(
+          word: 'sand',
+          start: Location(x: 2, y: -2),
+          direction: Direction.across,
+        );
+
+        expect(
+          board.connections(usa),
+          <Location>{
+            Location(x: 1, y: -2),
+          },
+        );
+      });
+
+      test('gets 3 connections for usa across at (2, -2)', () {
+        final board = Crossword1();
+
+        final usa = WordEntry(
+          word: 'egg',
+          start: Location(x: 1, y: -1),
+          direction: Direction.across,
+        );
+
+        expect(
+          board.connections(usa),
+          <Location>{
+            Location(x: 1, y: -2),
+            Location(x: 0, y: -1),
+            Location(x: 2, y: -2),
+          },
+        );
+      });
+    });
+
+    group('constraints', () {
+      test('returns null if neighboring word has matching direction', () {
+        final board = Crossword1();
+
+        final candidate = WordCandidate(
+          location: Location(x: 1, y: -2),
+          direction: Direction.down,
+        );
+
+        final constraints = board.constraints(candidate);
+        expect(constraints, isNull);
+      });
+
+      test('derives successfully a single constraint down', () {
+        final board = Crossword1();
+
+        final candidate = WordCandidate(
+          location: Location(x: 2, y: -2),
+          direction: Direction.down,
+        );
+
+        final constraints = board.constraints(candidate);
+        expect(
+          constraints,
+          equals(
+            ConstrainedWordCandidate(
+              invalidLengths: const {},
+              location: candidate.location,
+              direction: candidate.direction,
+              constraints: const {0: 's'},
+            ),
+          ),
+        );
+      });
+
+      test('derives successfully two constraints down', () {
+        final board = Crossword2();
+
+        final candidate = WordCandidate(
+          location: Location(x: 2, y: -2),
+          direction: Direction.down,
+        );
+
+        final constraints = board.constraints(candidate);
+        expect(
+          constraints,
+          equals(
+            ConstrainedWordCandidate(
+              invalidLengths: const {4},
+              location: candidate.location,
+              direction: candidate.direction,
+              constraints: const {0: 's', 4: 'w'},
+            ),
+          ),
+        );
+      });
+
+      test('derives successfully two constraints down with capped length', () {
+        final board = Crossword3();
+
+        final candidate = WordCandidate(
+          location: Location(x: 2, y: -2),
+          direction: Direction.down,
+        );
+
+        final constraints = board.constraints(candidate);
+        expect(
+          constraints,
+          equals(
+            ConstrainedWordCandidate(
+              invalidLengths: const {3, 4},
+              location: candidate.location,
+              direction: candidate.direction,
+              constraints: const {0: 's', 4: 'w'},
+            ),
+          ),
         );
       });
     });
