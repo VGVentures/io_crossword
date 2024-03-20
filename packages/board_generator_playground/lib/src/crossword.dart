8cc5a3d38ac805a37c87573334652d5144b55ec8
Alejandro Santiago <dev@alestiago.com>
2024-03-20 15:52:19 +0000

    feat: define toPrettyString

--- packages/board_generator_playground/lib/src/crossword.dart
+++ packages/board_generator_playground/lib/src/crossword.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:board_generator_playground/src/models/models.dart';
 
 /// {@template character_map}
@@ -369,5 +371,43 @@ class Crossword {
       direction: candidate.direction,
       constraints: constraints,
     );
+  }
+
+  /// A pretty string representation of the board.
+  ///
+  /// For example, a board with the words "ALBUS" and "BEHAN" would be:
+  ///
+  /// ```txt
+  /// ALBUS
+  /// --E--
+  /// --H--
+  /// --A--
+  /// --N--
+  /// ```
+  String toPrettyString() {
+    final stringBuffer = StringBuffer();
+
+    final minX =
+        characterMap.keys.map((e) => e.x).reduce((a, b) => a < b ? a : b);
+    final maxX =
+        characterMap.keys.map((e) => e.x).reduce((a, b) => a > b ? a : b);
+    final minY =
+        characterMap.keys.map((e) => e.y).reduce((a, b) => a < b ? a : b);
+    final maxY =
+        characterMap.keys.map((e) => e.y).reduce((a, b) => a > b ? a : b);
+
+    final width = maxX - minX + 1;
+
+    for (var row = minY; row <= maxY; row++) {
+      final characters = List.generate(width, (column) {
+        final location = Location(x: column + minX, y: row);
+        final character = (characterMap[location]?.character ?? '-').trim();
+        return character.toUpperCase();
+      });
+
+      stringBuffer.writeln(characters.join());
+    }
+
+    return stringBuffer.toString();
   }
 }
