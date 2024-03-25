0d9ced5f48ddd1240f59340e41483801cafc0c79
Alejandro Santiago <dev@alestiago.com>
2024-03-25 13:59:54 +0000

    feat: interface

--- packages/board_generator_playground/lib/scripts/simple_crossword.dart
+++ packages/board_generator_playground/lib/scripts/simple_crossword.dart
@@ -1,4 +1,3 @@
-import 'dart:collection';
 import 'dart:io';
 
 import 'package:board_generator_playground/src/crossword.dart';
@@ -6,120 +5,149 @@ import 'package:board_generator_playground/src/models/models.dart';
 import 'package:board_generator_playground/src/word_pool.dart';
 import 'package:csv/csv.dart';
 
-void main({
-  void Function(String) log = print,
-}) {
-  final fileString = File('words.csv').readAsStringSync();
-  final lines = const CsvToListConverter(eol: '\n').convert(fileString)
-    ..removeAt(0);
+/// {@template crossword_generator}
+///
+/// {@endtemplate}
+abstract class CrosswordGenerator {
+  /// {@macro crossword_generator}
+  CrosswordGenerator({
+    required this.pool,
+    required this.crossword,
+  });
 
-  final pool = <String>{
-    for (final line in lines) (line[0] as String).toLowerCase(),
-  };
-  log('Sorting ${pool.length} words');
-  final wordPool = WordPool(words: pool);
-  log('Sorted ${pool.length} words');
+  /// The word pool to draw from.
+  final WordPool pool;
 
-  final initialWorld = wordPool.firstMatch(
-    ConstrainedWordCandidate(
+  /// The crossword to populate.
+  final Crossword crossword;
+
+  /// The candidates yet to be expanded.
+  final Set<WordCandidate> candidates = {};
+
+  /// The candidates that have been expanded.
+  final Set<WordCandidate> closed = {};
+
+  /// Populates the crossword with words.
+  Crossword populate() {
+    seed();
+
+    do {
+      final candidate = nextCandidate;
+      if (candidate == null) break;
+
+      final entries = resolve(candidate);
+      if (entries == null) continue;
+
+      for (final entry in entries) {
+        add(entry);
+      }
+    } while (nextCandidate != null);
+
+    return crossword;
+  }
+
+  /// Seeds the generator with initial candidates.
+  void seed();
+
+  /// The next candidate to expand.
+  ///
+  /// Returns `null` if there are no candidates left.
+  WordCandidate? get nextCandidate {
+    if (candidates.isEmpty) return null;
+    final candidate = candidates.first;
+    return candidate;
+  }
+
+  /// Resolves the candidate to a different [WordEntry]s to be
+  /// placed on the board.
+  ///
+  /// If the candidate cannot be resolved, `null` is returned.
+  Set<WordEntry>? resolve(WordCandidate candidate);
+
+  /// Adds the entry to the crossword and expands it.
+  void add(WordEntry entry) {
+    crossword.add(entry);
+    pool.remove(entry.word);
+    final expansion = expand(entry)..removeWhere(closed.contains);
+    candidates.addAll(expansion);
+  }
+
+  /// Expands the candidate.
+  Set<WordCandidate> expand(WordEntry entry);
+}
+
+class _SimpleSquaredCrosswordGenerator extends CrosswordGenerator {
+  _SimpleSquaredCrosswordGenerator({
+    required super.pool,
+    required super.crossword,
+  });
+
+  @override
+  WordCandidate? get nextCandidate {
+    if (crossword.words.length > 1000) return null;
+    return super.nextCandidate;
+  }
+
+  @override
+  void seed() {
+    final constraints = ConstrainedWordCandidate(
       invalidLengths: {
-        for (int i = 2; i <= wordPool.longestWordLength; i += 2) i,
+        for (int i = 2; i <= pool.longestWordLength; i += 2) i,
       },
       start: Location.zero,
       direction: Direction.down,
       constraints: const {0: 'a'},
-    ),
-  )!;
-  log('Initial word: $initialWorld');
-
-  final initialWordEntry = WordEntry(
-    word: initialWorld,
-    start: Location(x: 0, y: 0 - (initialWorld.length ~/ 2)),
-    direction: Direction.down,
-  );
-  final crossword = Crossword(
-    bounds: Bounds.square(size: 100),
-  )..add(initialWordEntry);
-
-  var placedWords = 0;
-
-  final closed = <WordCandidate>{};
-
-  final frontier = Queue<WordCandidate>()
-    ..addAll(
-      {
-        for (final location in initialWordEntry.start.to(initialWordEntry.end))
-          WordCandidate(
-            start: location,
-            direction: initialWordEntry.direction == Direction.across
-                ? Direction.down
-                : Direction.across,
-          ),
-      },
     );
-
-  while (frontier.isNotEmpty && placedWords < 1000) {
-    final candidate = frontier.removeFirst();
-    closed.add(candidate);
-
-    if (crossword.crossesAt(candidate.start)) {
-      continue;
-    }
-
-    final wordCandidate = WordCandidate(
-      start: candidate.start,
-      direction: candidate.direction,
-    );
-    final constraints = crossword.constraints(wordCandidate);
-    if (constraints == null) continue;
-
-    final word = wordPool.firstMatch(constraints);
-    if (word == null) continue;
-
-    if (!constraints.satisfies(word)) {
-      log('Invalid candidate: $candidate');
-      continue;
-    }
-
-    final wordEntry = WordEntry(
+    final word = pool.firstMatch(constraints)!;
+    final entry = WordEntry(
       word: word,
-      start: candidate.start,
-      direction: candidate.direction,
+      start: Location(x: 0, y: 0 - (word.length ~/ 2)),
+      direction: constraints.direction,
     );
+    add(entry);
+    candidates.addAll(expand(entry));
+  }
 
-    crossword.add(wordEntry);
-    wordPool.remove(wordEntry.word);
+  @override
+  Set<WordEntry>? resolve(WordCandidate candidate) {
+    final constraints = crossword.constraints(candidate);
+    if (constraints == null) return null;
 
-    final span = wordEntry.start.to(wordEntry.end);
-    final expansion = {
-      for (final location in span)
-        WordCandidate(
-          start: location,
-          direction: wordEntry.direction == Direction.across
-              ? Direction.down
-              : Direction.across,
-        ),
+    final word = pool.firstMatch(constraints);
+    if (word == null) return null;
+
+    return {
+      WordEntry(
+        word: word,
+        start: candidate.start,
+        direction: candidate.direction,
+      ),
     };
+  }
 
-    final topLeft = wordEntry.direction == Direction.across
+  @override
+  Set<WordCandidate> expand(WordEntry entry) {
+    final expansion = <WordCandidate>{};
+    final topLeft = entry.direction == Direction.across
         ? Location(
-            x: wordEntry.start.x - wordPool.longestWordLength,
-            y: wordEntry.start.y,
+            x: entry.start.x - pool.longestWordLength,
+            y: entry.start.y,
           )
         : Location(
-            x: wordEntry.start.x,
-            y: wordEntry.start.y - wordPool.longestWordLength,
+            x: entry.start.x,
+            y: entry.start.y - pool.longestWordLength,
           );
-    final bottomRight = wordEntry.direction == Direction.across
+    final bottomRight = entry.direction == Direction.across
         ? Location(
-            x: wordEntry.end.x + wordPool.longestWordLength,
-            y: wordEntry.end.y,
+            x: entry.end.x + pool.longestWordLength,
+            y: entry.end.y,
           )
         : Location(
-            x: wordEntry.end.x,
-            y: wordEntry.end.y + wordPool.longestWordLength,
+            x: entry.end.x,
+            y: entry.end.y + pool.longestWordLength,
           );
     final radius = topLeft.to(bottomRight);
+
     for (final location in radius) {
       final words = crossword.wordsAt(location);
       if (!words.any((word) => word.direction == Direction.across)) {
@@ -139,14 +167,26 @@ void main({
         );
       }
     }
-    expansion.removeWhere(closed.contains);
-    frontier.addAll(expansion..removeWhere(closed.contains));
 
-    placedWords++;
-    if (placedWords % 200 == 0) {
-      log('Placed $placedWords words');
-    }
+    return expansion;
   }
+}
 
+void main({void Function(String) log = print}) {
+  final fileString = File('words.csv').readAsStringSync();
+  final lines = const CsvToListConverter(eol: '\n').convert(fileString)
+    ..removeAt(0);
+
+  final pool = <String>{
+    for (final line in lines) (line[0] as String).toLowerCase(),
+  };
+  final wordPool = WordPool(words: pool);
+
+  final generator = _SimpleSquaredCrosswordGenerator(
+    pool: wordPool,
+    crossword: Crossword(bounds: Bounds.square(size: 100)),
+  );
+
+  final crossword = generator.populate();
   File('crossword.txt').writeAsStringSync(crossword.toPrettyString());
 }
