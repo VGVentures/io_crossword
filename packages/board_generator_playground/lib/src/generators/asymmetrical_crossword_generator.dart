bd58fb08125019d0a08ea5467f6d9585583ddd4a
Alejandro Santiago <dev@alestiago.com>
2024-03-26 11:30:40 +0000

    feat: squaring board

--- packages/board_generator_playground/lib/src/generators/asymmetrical_crossword_generator.dart
+++ packages/board_generator_playground/lib/src/generators/asymmetrical_crossword_generator.dart
@@ -13,9 +13,26 @@ class AsymmetricalCrosswordGenerator extends CrosswordGenerator {
     required super.crossword,
   });
 
+  final Set<WordCandidate> _area = {};
+
+  final Set<Location> _area2 = {};
+
   @override
   WordCandidate? get nextCandidate {
-    if (crossword.words.length > 1000) return null;
+    if (crossword.words.length > 10000) return null;
+
+    if (candidates.isEmpty && _area.isNotEmpty) {
+      return null;
+
+      final candidates = {
+        for (final location in _area2)
+          WordCandidate(start: location, direction: Direction.across),
+        for (final location in _area2)
+          WordCandidate(start: location, direction: Direction.down),
+      }..removeWhere(closed.contains);
+      this.candidates.addAll(candidates);
+    }
+
     return super.nextCandidate;
   }
 
@@ -56,11 +73,60 @@ class AsymmetricalCrosswordGenerator extends CrosswordGenerator {
     };
   }
 
+  Set<WordCandidate> area(WordEntry entry) {
+    final span = entry.start.to(entry.end);
+    final topLeft = entry.direction == Direction.across
+        ? Location(
+            x: entry.start.x,
+            y: entry.start.y - pool.longestWordLength,
+          )
+        : Location(
+            x: entry.start.x - pool.longestWordLength,
+            y: entry.start.y,
+          );
+    final bottomRight = entry.direction == Direction.across
+        ? Location(
+            x: entry.end.x,
+            y: entry.end.y + pool.longestWordLength,
+          )
+        : Location(
+            x: entry.end.x + pool.longestWordLength,
+            y: entry.end.y,
+          );
+    final area = topLeft.to(bottomRight)
+      ..removeWhere(span.contains)
+      ..removeWhere(crossword.crossesAt);
+
+    final bounds = crossword.bounds;
+    if (bounds != null) {
+      area.removeWhere((location) => !bounds.contains(location));
+    }
+
+    final candidates = <WordCandidate>{};
+    for (final location in area) {
+      final words = crossword.wordsAt(location);
+      if (!words.any((word) => word.direction == Direction.across)) {
+        candidates
+            .add(WordCandidate(start: location, direction: Direction.across));
+      }
+      if (!words.any((word) => word.direction == Direction.down)) {
+        candidates
+            .add(WordCandidate(start: location, direction: Direction.down));
+      }
+    }
+    candidates.removeWhere(closed.contains);
+
+    return candidates;
+  }
+
   @override
   Set<WordCandidate> expand(WordEntry entry) {
     final span = entry.start.to(entry.end);
     final newDirection =
         entry.direction == Direction.down ? Direction.across : Direction.down;
+
+    _area.addAll(area(entry));
+
     return {
       for (final location in span)
         WordCandidate(
