ec2606a0e3f250d1d4e9c7737f7743d32feaa2fc
AyadLaouissi <ayadlaouissi@gmail.com>
2024-04-01 13:35:58 +0200

    added about_links.dart

--- lib/about/view/about_view.dart
+++ lib/about/view/about_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:io_crossword/about/link/about_links.dart';
 import 'package:io_crossword/extensions/extensions.dart';
 import 'package:io_crossword/l10n/l10n.dart';
 import 'package:io_crossword_ui/io_crossword_ui.dart';
