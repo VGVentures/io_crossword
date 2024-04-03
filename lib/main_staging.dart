e6c4a06a9de4d8ef5e036de7b0a9930391de0f6a
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-04-03 19:50:43 +0200

    fix: appcheck initialization in staging

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -7,6 +7,7 @@ import 'package:api_client/api_client.dart';
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:board_info_repository/board_info_repository.dart';
 import 'package:crossword_repository/crossword_repository.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
@@ -29,6 +30,13 @@ void main() async {
   unawaited(
     bootstrap(
       (firestore, firebaseAuth, appCheck) async {
+        await appCheck.activate(
+          webProvider: ReCaptchaV3Provider(
+            const String.fromEnvironment('RECAPTCHA_KEY'),
+          ),
+        );
+        await appCheck.setTokenAutoRefreshEnabled(true);
+
         final authenticationRepository = AuthenticationRepository(
           firebaseAuth: firebaseAuth,
         );
