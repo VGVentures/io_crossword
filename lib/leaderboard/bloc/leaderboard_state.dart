cd0fa9ab45a12b153c62b735e974ccf9781b8cb9
Ayad Laaouissi Jones <42848220+AyadLaouissi@users.noreply.github.com>
2024-04-05 11:39:31 +0200

    feat: creates leaderboard bloc (#197)

---
+++ lib/leaderboard/bloc/leaderboard_state.dart
@@ -0,0 +1,31 @@
+part of 'leaderboard_bloc.dart';
+
+enum LeaderboardStatus {
+  initial,
+  success,
+  empty,
+  failure,
+}
+
+class LeaderboardState extends Equatable {
+  const LeaderboardState({
+    this.status = LeaderboardStatus.initial,
+    this.players = const [],
+  });
+
+  final LeaderboardStatus status;
+  final List<LeaderboardPlayer> players;
+
+  LeaderboardState copyWith({
+    LeaderboardStatus? status,
+    List<LeaderboardPlayer>? players,
+  }) {
+    return LeaderboardState(
+      status: status ?? this.status,
+      players: players ?? this.players,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, players];
+}
