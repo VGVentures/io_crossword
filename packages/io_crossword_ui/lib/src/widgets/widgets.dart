afcd9967db583dcfb5bb6d496437bdb955a0838f
Alejandro Santiago <dev@alestiago.com>
2024-04-06 09:44:42 +0100

    feat: export

--- packages/io_crossword_ui/lib/src/widgets/widgets.dart
+++ packages/io_crossword_ui/lib/src/widgets/widgets.dart
@@ -1,6 +1,7 @@
 export 'card_scrollable_content_with_button.dart';
 export 'gemini_gradient.dart';
 export 'io_crossword_card.dart';
+export 'io_layout.dart';
 export 'io_linear_progress_indicator.dart';
 export 'io_logo.dart';
 export 'io_physical_model.dart';
