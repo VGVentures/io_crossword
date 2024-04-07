72af9178bccf6cb727e5f85807d514d8d29f6171
Alejandro Santiago <dev@alestiago.com>
2024-04-07 09:46:18 +0100

    chore: remove redundant import

--- lib/welcome/view/welcome_page.dart
+++ lib/welcome/view/welcome_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:io_crossword/l10n/l10n.dart';
 import 'package:io_crossword/welcome/welcome.dart';
-import 'package:io_crossword_ui/io_crossword_ui.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
