e5fbb1d2099bafa1968a369ee969789e2f51aa98
Ayad Laaouissi Jones <42848220+AyadLaouissi@users.noreply.github.com>
2024-04-09 13:29:23 +0200

    feat: adds app bar to welcome and main page (#223)
    * feat: adds app bar to welcome and main page
    
    * updated
    
    * small update
    
    * updated music_icon_button.dart
    
    * PR review updates
    
    * small update
    
    * Update lib/crossword/view/crossword_page.dart
    
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>
    
    ---------
    
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>

--- packages/io_crossword_ui/lib/src/widgets/io_app_bar.dart
+++ packages/io_crossword_ui/lib/src/widgets/io_app_bar.dart
@@ -11,7 +11,8 @@ class IoAppBar extends StatelessWidget implements PreferredSizeWidget {
   const IoAppBar({
     required this.crossword,
     required this.actions,
-    required this.title,
+    this.title,
+    this.bottom,
     super.key,
   });
 
@@ -22,10 +23,21 @@ class IoAppBar extends StatelessWidget implements PreferredSizeWidget {
   final WidgetBuilder actions;
 
   /// The title of the app bar.
-  final Widget title;
+  ///
+  /// If null will always display [_IoCrosswordLogo].
+  final Widget? title;
+
+  /// This widget appears across the bottom of the app bar.
+  ///
+  ///  * [PreferredSize], which can be used to give an arbitrary widget
+  ///  a preferred size.
+  final PreferredSizeWidget? bottom;
 
   @override
-  Size get preferredSize => const Size(double.infinity, 80);
+  Size get preferredSize => Size(
+        double.infinity,
+        80 + (bottom?.preferredSize.height ?? 0),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -35,54 +47,89 @@ class IoAppBar extends StatelessWidget implements PreferredSizeWidget {
       style: Theme.of(context).textTheme.bodyLarge!.copyWith(
             fontWeight: FontWeight.w500,
           ),
-      child: title,
+      child: title ?? const SizedBox(),
     );
 
+    final widget = Material(
+      color: Theme.of(context).colorScheme.surface,
+      child: Column(
+        children: [
+          const Spacer(),
+          SizedBox(
+            height: 40,
+            child: switch (layout) {
+              IoLayoutData.small => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      if (title == null)
+                        _IoCrosswordLogo(
+                          crossword: crossword,
+                        )
+                      else
+                        titleWidget,
+                      actions(context),
+                    ],
+                  ),
+                ),
+              IoLayoutData.large => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _IoCrosswordLogo(
+                        crossword: crossword,
+                      ),
+                      titleWidget,
+                      actions(context),
+                    ],
+                  ),
+                ),
+            },
+          ),
+          const Spacer(),
+          const Divider(height: 0),
+        ],
+      ),
+    );
+
+    final bottom = this.bottom;
+
+    if (bottom == null) return widget;
+
     return Column(
-      children: [
-        const Spacer(),
-        SizedBox(
-          height: 40,
-          child: switch (layout) {
-            IoLayoutData.small => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    titleWidget,
-                    actions(context),
-                  ],
-                ),
-              ),
-            IoLayoutData.large => Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const IoLogo(),
-                        const SizedBox(width: 5),
-                        Text(
-                          crossword,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineSmall
-                              ?.copyWith(
-                                fontWeight: FontWeight.w700,
-                              ),
-                        ),
-                      ],
-                    ),
-                    titleWidget,
-                    actions(context),
-                  ],
-                ),
-              ),
-          },
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        ConstrainedBox(
+          constraints: const BoxConstraints(maxHeight: 80),
+          child: widget,
         ),
-        const Spacer(),
-        const Divider(height: 0),
+        bottom,
+      ],
+    );
+  }
+}
+
+class _IoCrosswordLogo extends StatelessWidget {
+  const _IoCrosswordLogo({
+    required this.crossword,
+  });
+
+  final String crossword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const IoLogo(),
+        const SizedBox(width: 5),
+        Text(
+          crossword,
+          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                fontWeight: FontWeight.w700,
+              ),
+        ),
       ],
     );
   }
