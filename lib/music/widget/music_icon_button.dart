9ac96ef7cc3388ceb9379eab37f611978d642c11
AyadLaouissi <ayadlaouissi@gmail.com>
2024-04-09 12:52:44 +0200

    updated music_icon_button.dart

--- lib/music/widget/music_icon_button.dart
+++ lib/music/widget/music_icon_button.dart
@@ -1,6 +1,6 @@
 // TODO(Ayad): volume logic
 // https://very-good-ventures-team.monday.com/boards/6004820050/pulses/6417645325
-// coverage: ignore-file
+// coverage:ignore-file
 import 'package:flutter/material.dart';
 
 class MusicIconButton extends StatelessWidget {
