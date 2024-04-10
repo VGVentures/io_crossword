2d24aa99a3e0e44f976361029f0915059e378e3a
Ayad Laaouissi Jones <42848220+AyadLaouissi@users.noreply.github.com>
2024-04-10 12:39:37 +0200

    feat: adds google icon (#242)

--- packages/io_crossword_ui/lib/src/icons/io_icons_icons.g.dart
+++ packages/io_crossword_ui/lib/src/icons/io_icons_icons.g.dart
@@ -11,4 +11,6 @@ class IoIcons {
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData icon_right =
       IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData google =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
