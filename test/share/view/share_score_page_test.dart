6ff55579b1b77d79baac6548ac20eea7c67915dd
Hugo Walbecq <hugo@verygood.ventures>
2024-04-11 12:29:41 +0200

    test: show modal test

--- test/share/view/share_score_page_test.dart
+++ test/share/view/share_score_page_test.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_crossword/share/view/share_score_page.dart';
 import 'package:io_crossword_ui/io_crossword_ui.dart';
@@ -14,6 +15,28 @@ void main() {
         await tester.pumpApp(ShareScorePage());
 
         expect(find.byType(IoCrosswordCard), findsOneWidget);
+      },
+    );
+
+    testWidgets(
+      'showModal opens the ShareScorePage',
+      (tester) async {
+        // Build the test widget
+        await tester.pumpApp(Scaffold(
+          body: Builder(
+            builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () => ShareScorePage.showModal(context),
+                child: Text('Show ShareScorePage'),
+              );
+            },
+          ),
+        ));
+
+        await tester.tap(find.byType(ElevatedButton));
+        await tester.pumpAndSettle();
+
+        expect(find.byType(ShareScorePage), findsOneWidget);
       },
     );
   });
