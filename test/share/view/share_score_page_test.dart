762ef23c1e5b3d991f30373abfb89d121220c843
Hugo Walbecq <hugo@verygood.ventures>
2024-04-11 12:33:17 +0200

    chore: analysis....

--- test/share/view/share_score_page_test.dart
+++ test/share/view/share_score_page_test.dart
@@ -22,16 +22,18 @@ void main() {
       'showModal opens the ShareScorePage',
       (tester) async {
         // Build the test widget
-        await tester.pumpApp(Scaffold(
-          body: Builder(
-            builder: (BuildContext context) {
-              return ElevatedButton(
-                onPressed: () => ShareScorePage.showModal(context),
-                child: Text('Show ShareScorePage'),
-              );
-            },
+        await tester.pumpApp(
+          Scaffold(
+            body: Builder(
+              builder: (BuildContext context) {
+                return ElevatedButton(
+                  onPressed: () => ShareScorePage.showModal(context),
+                  child: Text('Show ShareScorePage'),
+                );
+              },
+            ),
           ),
-        ));
+        );
 
         await tester.tap(find.byType(ElevatedButton));
         await tester.pumpAndSettle();
