e55e8a0c58ec0196c7c2a3d88930331266370060
Alejandro Santiago <dev@alestiago.com>
2024-04-12 10:29:01 +0100

    feat: adjust WordClueView

--- lib/word_focused/view/word_clue_view.dart
+++ lib/word_focused/view/word_clue_view.dart
@@ -12,7 +12,6 @@ class WordClueDesktopView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
     final solved = selectedWord.solvedStatus == WordStatus.solved;
 
     return Column(
@@ -27,14 +26,7 @@ class WordClueDesktopView extends StatelessWidget {
         ),
         const Spacer(),
         const SizedBox(height: 8),
-        if (!solved)
-          OutlinedButton.icon(
-            onPressed: () {
-              context.read<WordSelectionBloc>().add(const WordSolveRequested());
-            },
-            icon: const Icon(Icons.edit),
-            label: Text(l10n.solveIt),
-          ),
+        if (!solved) _SolveItButton(wordIdentifier: selectedWord.word.id),
       ],
     );
   }
@@ -47,7 +39,6 @@ class WordClueMobileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
     final solved = selectedWord.solvedStatus == WordStatus.solved;
 
     return Column(
@@ -61,16 +52,34 @@ class WordClueMobileView extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
         const SizedBox(height: 24),
-        if (!solved)
-          OutlinedButton.icon(
-            onPressed: () {
-              context.read<WordSelectionBloc>().add(const WordSolveRequested());
-            },
-            icon: const Icon(Icons.edit),
-            label: Text(l10n.solveIt),
-          ),
+        if (!solved) _SolveItButton(wordIdentifier: selectedWord.word.id),
         const SizedBox(height: 16),
       ],
+    );
+  }
+}
+
+class _SolveItButton extends StatelessWidget {
+  const _SolveItButton({
+    required this.wordIdentifier,
+  });
+
+  final String wordIdentifier;
+
+  void _onSolveIt(BuildContext context) {
+    context.read<WordSelectionBloc>().add(
+          WordSolveRequested(wordIdentifier: wordIdentifier),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+
+    return OutlinedButton.icon(
+      onPressed: () => _onSolveIt(context),
+      icon: const Icon(Icons.edit),
+      label: Text(l10n.solveIt),
     );
   }
 }
