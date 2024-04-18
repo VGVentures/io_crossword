6c04cb342dd933a0c55ceddf65df6c5ee478080d
AyadLaouissi <ayadlaouissi@gmail.com>
2024-04-18 12:38:33 +0200

    feat: adds reset api call

---
+++ api/test/routes/game/reset_streak_test.dart
@@ -0,0 +1,81 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:jwt_middleware/jwt_middleware.dart';
+import 'package:leaderboard_repository/leaderboard_repository.dart';
+import 'package:logging/logging.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../../../routes/game/reset_streak.dart' as route;
+
+class _MockLeaderboardRepository extends Mock
+    implements LeaderboardRepository {}
+
+class _MockRequest extends Mock implements Request {}
+
+class _MockRequestContext extends Mock implements RequestContext {}
+
+class _MockLogger extends Mock implements Logger {}
+
+void main() {
+  late LeaderboardRepository leaderboardRepository;
+  late Request request;
+  late RequestContext context;
+  late Logger logger;
+
+  setUp(() {
+    leaderboardRepository = _MockLeaderboardRepository();
+    request = _MockRequest();
+    logger = _MockLogger();
+
+    context = _MockRequestContext();
+    when(() => context.request).thenReturn(request);
+    when(() => context.read<LeaderboardRepository>())
+        .thenReturn(leaderboardRepository);
+    when(() => context.read<Logger>()).thenReturn(logger);
+    when(() => context.read<AuthenticatedUser>())
+        .thenReturn(AuthenticatedUser('id'));
+  });
+
+  group('POST', () {
+    test(
+      'responds with a HttpStatus.created status when reset is correct',
+      () async {
+        when(() => request.method).thenReturn(HttpMethod.post);
+        when(() => leaderboardRepository.resetStreak('id'))
+            .thenAnswer((_) async {});
+
+        final response = await route.onRequest(context);
+        expect(response.statusCode, equals(HttpStatus.created));
+      },
+    );
+
+    test(
+      'calls resetStreak with the user id',
+      () async {
+        when(() => request.method).thenReturn(HttpMethod.post);
+        when(() => leaderboardRepository.resetStreak('id'))
+            .thenAnswer((_) async {});
+
+        await route.onRequest(context);
+
+        verify(() => leaderboardRepository.resetStreak('id')).called(1);
+      },
+    );
+
+    test(
+      'throws error when resetStreak throws exception',
+      () async {
+        when(() => request.method).thenReturn(HttpMethod.post);
+        when(() => leaderboardRepository.resetStreak('id'))
+            .thenThrow(Exception());
+
+        final response = route.onRequest(context);
+        expect(response, throwsA(isA<Exception>()));
+      },
+    );
+  });
+}
