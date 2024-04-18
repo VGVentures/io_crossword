1801cb0f50fc2b36049ac9e1501f7ea8829a2263
Alejandro Santiago <dev@alestiago.com>
2024-04-18 16:18:03 +0100

    docs: ()

--- lib/crossword2/crossword2.dart
+++ lib/crossword2/crossword2.dart
@@ -1,6 +1,6 @@
 /// Shows the crossword puzzle.
 // NOTE: This is currently not being integrated directly within the
-// application, but it will hopefully be integrated in the future. As
+// application, but it will hopefully be integrated in the future (as
 // soon as this Flutter-based approach reaches feature parity with the
-// existing Flame-based approach.
+// existing Flame-based approach).
 library;
