a86be8b79945ff8f9b925650cdc7988b70dcb1e9
Alejandro Santiago <dev@alestiago.com>
2024-04-18 15:54:42 +0100

    chore: add Flutter based crossword directory

---
+++ lib/crossword2/crossword2.dart
@@ -0,0 +1,9 @@
+/// Shows the crossword puzzle.
+// NOTE: This is a currently not being integrated directly within the
+// application, but it will hopefully be integrated in the future. As
+// soon as this Flutter-based approach reaches feature parity with the
+// existing Flame-based approach.
+library;
+
+export 'views/views.dart';
+export 'widgets/widgets.dart';
