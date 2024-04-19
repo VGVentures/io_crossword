a4a32357ec56c0d9c07fbcdb18d87c6eb48f562c
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-04-19 12:18:36 +0200

    feat: use hint bloc in word selection page

---
+++ lib/word_selection/view/word_selection_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:io_crossword/hint/bloc/hint_bloc.dart';
+import 'package:io_crossword/word_selection/word_selection.dart';
+
+class WordSelectionPage extends StatelessWidget {
+  const WordSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedWord =
+        context.select((WordSelectionBloc bloc) => bloc.state.word);
+    if (selectedWord == null) return const SizedBox.shrink();
+
+    return BlocProvider(
+      create: (context) => HintBloc(),
+      child: const WordSelectionView(),
+    );
+  }
+}
