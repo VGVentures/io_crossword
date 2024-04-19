3f9a2af8b31eeb8102dab0652f9dad73393a0ab8
Ayad Laaouissi Jones <42848220+AyadLaouissi@users.noreply.github.com>
2024-04-19 10:29:55 +0200

    feat: adds streak at risk validation on close_word_selection_icon_button.dart (#327)

--- test/word_selection/widgets/close_word_selection_icon_button_test.dart
+++ test/word_selection/widgets/close_word_selection_icon_button_test.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:game_domain/game_domain.dart';
 import 'package:io_crossword/crossword/crossword.dart';
+import 'package:io_crossword/player/player.dart';
+import 'package:io_crossword/streak/streak.dart';
 import 'package:io_crossword/word_selection/word_selection.dart'
     hide WordUnselected;
 import 'package:io_crossword/word_selection/word_selection.dart' as selection;
@@ -16,14 +21,19 @@ class _MockWordSelectionBloc
     extends MockBloc<WordSelectionEvent, WordSelectionState>
     implements WordSelectionBloc {}
 
+class _MockPlayerBloc extends MockBloc<PlayerEvent, PlayerState>
+    implements PlayerBloc {}
+
 void main() {
   group('$CloseWordSelectionIconButton', () {
     late CrosswordBloc crosswordBloc;
     late WordSelectionBloc wordSelectionBloc;
+    late PlayerBloc playerBloc;
 
     setUp(() {
       crosswordBloc = _MockCrosswordBloc();
       wordSelectionBloc = _MockWordSelectionBloc();
+      playerBloc = _MockPlayerBloc();
     });
 
     testWidgets('renders $CloseWordSelectionIconButton', (tester) async {
@@ -32,7 +42,8 @@ void main() {
       expect(find.byType(CloseWordSelectionIconButton), findsOneWidget);
     });
 
-    testWidgets('emits $WordUnselected when tapped', (tester) async {
+    testWidgets('emits $WordUnselected when tapped with player streak at 0',
+        (tester) async {
       await tester.pumpApp(
         crosswordBloc: crosswordBloc,
         BlocProvider(
@@ -47,6 +58,59 @@ void main() {
       verify(() => crosswordBloc.add(const WordUnselected())).called(1);
       verify(() => wordSelectionBloc.add(const selection.WordUnselected()))
           .called(1);
+    });
+
+    testWidgets(
+        'does not add $WordUnselected when tapped '
+        'with player streak at 1 and word solving in progress', (tester) async {
+      when(() => playerBloc.state).thenReturn(
+        PlayerState(player: Player.empty.copyWith(streak: 1)),
+      );
+      when(() => wordSelectionBloc.state).thenReturn(
+        WordSelectionState(status: WordSelectionStatus.solving),
+      );
+
+      await tester.pumpApp(
+        crosswordBloc: crosswordBloc,
+        playerBloc: playerBloc,
+        BlocProvider(
+          create: (_) => wordSelectionBloc,
+          child: const CloseWordSelectionIconButton(),
+        ),
+      );
+
+      await tester.tap(find.byType(CloseWordSelectionIconButton));
+      await tester.pumpAndSettle();
+
+      verifyNever(() => crosswordBloc.add(const WordUnselected()));
+      verifyNever(
+        () => wordSelectionBloc.add(const selection.WordUnselected()),
+      );
+    });
+
+    testWidgets(
+        'displays StreakAtRiskView when tapped '
+        'with player streak at 1 and word solving in progress', (tester) async {
+      when(() => playerBloc.state).thenReturn(
+        PlayerState(player: Player.empty.copyWith(streak: 1)),
+      );
+      when(() => wordSelectionBloc.state).thenReturn(
+        WordSelectionState(status: WordSelectionStatus.solving),
+      );
+
+      await tester.pumpApp(
+        crosswordBloc: crosswordBloc,
+        playerBloc: playerBloc,
+        BlocProvider(
+          create: (_) => wordSelectionBloc,
+          child: const CloseWordSelectionIconButton(),
+        ),
+      );
+
+      await tester.tap(find.byType(CloseWordSelectionIconButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(StreakAtRiskView), findsOneWidget);
     });
   });
 }
