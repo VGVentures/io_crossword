5d2858c808d558ae7cd6c2a376f5d0ce4c2d144d
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-04-19 14:33:45 +0200

    fix: styles

--- lib/initials/widgets/initials_submit_button.dart
+++ lib/initials/widgets/initials_submit_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:io_crossword/l10n/l10n.dart';
+import 'package:io_crossword_ui/io_crossword_ui.dart';
 
 class InitialsSubmitButton extends StatelessWidget {
   const InitialsSubmitButton({
@@ -12,10 +13,18 @@ class InitialsSubmitButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+    final theme = Theme.of(context);
+    final layout = IoLayout.of(context);
 
     return OutlinedButton(
       onPressed: onPressed,
-      child: Text(l10n.enter),
+      child: Text(
+        l10n.enter,
+        style: switch (layout) {
+          IoLayoutData.small => theme.textTheme.bodySmall,
+          IoLayoutData.large => theme.textTheme.bodyMedium,
+        },
+      ),
     );
   }
 }
