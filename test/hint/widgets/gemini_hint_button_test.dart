e49614db92b9a2ce3cfee0a43249f23ae7ff4b92
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-04-19 17:11:13 +0200

    test: gemini buttons

--- test/hint/widgets/gemini_hint_button_test.dart
+++ test/hint/widgets/gemini_hint_button_test.dart
@@ -1,19 +1,30 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_crossword/hint/hint.dart';
 import 'package:io_crossword/l10n/l10n.dart';
 import 'package:io_crossword_ui/io_crossword_ui.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/helpers.dart';
+
+class _MockHintBloc extends MockBloc<HintEvent, HintState>
+    implements HintBloc {}
 
 void main() {
   group('$GeminiHintButton', () {
     late AppLocalizations l10n;
+    late HintBloc hintBloc;
 
     setUpAll(() async {
       l10n = await AppLocalizations.delegate.load(Locale('en'));
+    });
+
+    setUp(() {
+      hintBloc = _MockHintBloc();
     });
 
     testWidgets('displays hint text', (tester) async {
@@ -26,6 +37,19 @@ void main() {
       await tester.pumpApp(GeminiHintButton());
 
       expect(find.byIcon(IoIcons.gemini), findsOneWidget);
+    });
+
+    testWidgets('emits HintModeEntered when tapped', (tester) async {
+      await tester.pumpApp(
+        BlocProvider.value(
+          value: hintBloc,
+          child: GeminiHintButton(),
+        ),
+      );
+
+      await tester.tap(find.byType(GeminiHintButton));
+
+      verify(() => hintBloc.add(const HintModeEntered())).called(1);
     });
   });
 }
--- test/hint/widgets/gemini_text_field_test.dart
+++ test/hint/widgets/gemini_text_field_test.dart
@@ -1,19 +1,30 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_crossword/hint/hint.dart';
 import 'package:io_crossword/l10n/l10n.dart';
 import 'package:io_crossword_ui/io_crossword_ui.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/helpers.dart';
+
+class _MockHintBloc extends MockBloc<HintEvent, HintState>
+    implements HintBloc {}
 
 void main() {
   group('$GeminiTextField', () {
     late AppLocalizations l10n;
+    late HintBloc hintBloc;
 
     setUpAll(() async {
       l10n = await AppLocalizations.delegate.load(Locale('en'));
+    });
+
+    setUp(() {
+      hintBloc = _MockHintBloc();
     });
 
     testWidgets('displays type hint', (tester) async {
@@ -33,5 +44,21 @@ void main() {
 
       expect(find.byIcon(Icons.send), findsOneWidget);
     });
+
+    testWidgets(
+      'sends HintRequested when tapping the send button',
+      (tester) async {
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: hintBloc,
+            child: GeminiTextField(),
+          ),
+        );
+
+        await tester.tap(find.byIcon(Icons.send));
+
+        verify(() => hintBloc.add(const HintRequested('is it red?'))).called(1);
+      },
+    );
   });
 }
