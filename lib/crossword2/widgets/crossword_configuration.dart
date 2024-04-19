6775c312015f814bc60d3e5f31e8c1df6fb79927
Alejandro Santiago <dev@alestiago.com>
2024-04-19 09:04:04 +0100

    docs: update bottomLeft

--- lib/crossword2/widgets/crossword_configuration.dart
+++ lib/crossword2/widgets/crossword_configuration.dart
@@ -45,8 +45,8 @@ class CrosswordConfiguration extends Equatable {
 
   /// The index of the bottom-left chunk of the crossword.
   ///
-  /// Since the [topLeft] is (0,0), the bottom-left may represent the
-  /// number of chunks in the crossword.
+  /// Since the [topLeft] is (0,0), the bottom-left may be used to easily derive
+  /// the number of chunks in the crossword.
   final CrosswordChunkIndex bottomLeft;
 
   /// {@macro crossword_chunk_size}
