b548144ff152ce6947f24a67a4c6805e11d77c94
Alejandro Santiago <dev@alestiago.com>
2024-04-19 11:14:09 +0100

    feat: define CrosswordStack

--- lib/crossword2/view/crossword2_view.dart
+++ lib/crossword2/view/crossword2_view.dart
@@ -7,7 +7,7 @@ class Crossword2View extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const configuration = CrosswordConfiguration(
-      bottomLeft: (40, 40),
+      bottomLeft: (45, 45),
       chunkSize: 20,
     );
 
@@ -16,9 +16,57 @@ class Crossword2View extends StatelessWidget {
         configuration: configuration,
         cellSize: const Size.square(20),
       ),
-      child: const CrosswordChunk(
-        index: CrosswordConfiguration.topLeft,
+      child: CrosswordInteractiveViewer(
+        builder: (_, __) => const _CrosswordStack(configuration: configuration),
       ),
     );
   }
+}
+
+class _CrosswordStack extends StatelessWidget {
+  const _CrosswordStack({required this.configuration});
+
+  final CrosswordConfiguration configuration;
+
+  @override
+  Widget build(BuildContext context) {
+    final crosswordLayout = CrosswordLayoutScope.of(context);
+    final quad = QuadScope.of(context);
+    final viewport = quad.toRect();
+
+    bool isChunkVisible(CrosswordChunkIndex index) {
+      final chunkRect = Rect.fromLTWH(
+        index.$1 * crosswordLayout.chunkSize.width,
+        index.$2 * crosswordLayout.chunkSize.height,
+        crosswordLayout.chunkSize.width,
+        crosswordLayout.chunkSize.height,
+      );
+      return chunkRect.overlaps(viewport);
+    }
+
+    final visibleChunks = <CrosswordChunkIndex>{
+      for (var row = 0; row <= configuration.bottomLeft.$1; row++)
+        for (var column = 0; column <= configuration.bottomLeft.$2; column++)
+          if (isChunkVisible((row, column))) (row, column),
+    };
+
+    return SizedBox.fromSize(
+      size: crosswordLayout.crosswordSize,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          for (final chunk in visibleChunks)
+            Positioned(
+              left: chunk.$1 * crosswordLayout.chunkSize.width,
+              top: chunk.$2 * crosswordLayout.chunkSize.height,
+              child: CrosswordChunk(index: chunk),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+extension on Quad {
+  Rect toRect() => Rect.fromLTRB(point0.x, point0.y, point2.x, point2.y);
 }
