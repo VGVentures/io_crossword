783fb33174fb43ccf97c5942c325cc59c4fd6c43
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-04-22 20:18:18 +0200

    chore: update env variables for deployments

--- scripts/start_local_api.sh
+++ scripts/start_local_api.sh
@@ -3,10 +3,10 @@
 export FB_APP_ID=io-crossword-dev
 export GAME_URL=http://localhost:24514
 export USE_EMULATOR=false
-export ENCRYPTION_KEY=X9YTchZdcnyZTNBSBgzj29p7RMBAIubD
-export ENCRYPTION_IV=FxC21ctRg9SgiXuZ
 export INITIALS_BLACKLIST_ID=T1ilfCwjDpLS7iaFzenA
 export FB_STORAGE_BUCKET=io-crossword-dev.appspot.com
+export GEMINI_API_KEY=$GEMINI_API_KEY
+export GEMINI_MODEL=gemini-pro
 
 echo ' ######################## '
 echo ' ## Starting dart frog ## '
