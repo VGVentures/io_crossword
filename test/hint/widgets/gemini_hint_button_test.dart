a0606bccbed95ad7af793b686888ba24f7fa3d42
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-04-22 12:44:01 +0200

    feat: disable hint button when there are no hints remaining

--- test/hint/widgets/gemini_hint_button_test.dart
+++ test/hint/widgets/gemini_hint_button_test.dart
@@ -1,9 +1,11 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_redundant_argument_values
+// ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:game_domain/game_domain.dart';
 import 'package:io_crossword/hint/hint.dart';
 import 'package:io_crossword/l10n/l10n.dart';
 import 'package:io_crossword_ui/io_crossword_ui.dart';
@@ -25,6 +27,7 @@ void main() {
 
     setUp(() {
       hintBloc = _MockHintBloc();
+      when(() => hintBloc.state).thenReturn(const HintState());
     });
 
     testWidgets('displays hint text', (tester) async {
@@ -39,17 +42,42 @@ void main() {
       expect(find.byIcon(IoIcons.gemini), findsOneWidget);
     });
 
-    testWidgets('emits HintModeEntered when tapped', (tester) async {
-      await tester.pumpApp(
-        BlocProvider.value(
-          value: hintBloc,
-          child: GeminiHintButton(),
-        ),
-      );
+    testWidgets(
+      'emits HintModeEntered when tapped if there are hints left',
+      (tester) async {
+        when(() => hintBloc.state).thenReturn(
+          HintState(hints: [], maxHints: 3),
+        );
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: hintBloc,
+            child: GeminiHintButton(),
+          ),
+        );
 
-      await tester.tap(find.byType(GeminiHintButton));
+        await tester.tap(find.byType(GeminiHintButton));
 
-      verify(() => hintBloc.add(const HintModeEntered())).called(1);
-    });
+        verify(() => hintBloc.add(const HintModeEntered())).called(1);
+      },
+    );
+
+    testWidgets(
+      'is disabled when there are no hints left',
+      (tester) async {
+        final hint = Hint(question: 'is it orange?', response: HintResponse.no);
+        when(() => hintBloc.state).thenReturn(
+          HintState(hints: [hint, hint, hint], maxHints: 3),
+        );
+        await tester.pumpApp(
+          BlocProvider.value(
+            value: hintBloc,
+            child: GeminiHintButton(),
+          ),
+        );
+        await tester.tap(find.byType(GeminiHintButton));
+
+        verifyNever(() => hintBloc.add(const HintModeEntered()));
+      },
+    );
   });
 }
