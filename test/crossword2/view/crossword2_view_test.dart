02c1b78d042e71454bd4ff0bff28e7c8a2da2709
Alejandro Santiago <dev@alestiago.com>
2024-04-29 11:42:40 +0100

    chore: dart fix

--- test/crossword2/view/crossword2_view_test.dart
+++ test/crossword2/view/crossword2_view_test.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:game_domain/game_domain.dart' as domain show Axis;
