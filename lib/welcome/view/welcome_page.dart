b17d2cd08e19d5e3ec0edc0321b73179caa58598
Hugo Walbecq <hugo@verygood.ventures>
2024-05-02 12:52:40 +0200

    chore: remove IoScaffold from welcome page

--- lib/welcome/view/welcome_page.dart
+++ lib/welcome/view/welcome_page.dart
@@ -6,7 +6,6 @@ import 'package:io_crossword/challenge/challenge.dart';
 import 'package:io_crossword/game_intro/game_intro.dart';
 import 'package:io_crossword/l10n/l10n.dart';
 import 'package:io_crossword/welcome/welcome.dart';
-import 'package:io_crossword/widget/io_scaffold.dart';
 import 'package:io_crossword_ui/io_crossword_ui.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -43,8 +42,13 @@ class WelcomeLarge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IoScaffold(
-      child: SelectionArea(
+    final l10n = context.l10n;
+
+    return Scaffold(
+      appBar: IoAppBar(
+        crossword: l10n.crossword,
+      ),
+      body: SelectionArea(
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
