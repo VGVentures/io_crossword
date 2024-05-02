b9d32ce76ce7e2d8c722030e7bc8f286fca7ae7d
Hugo Walbecq <hugo@verygood.ventures>
2024-05-02 10:00:37 +0200

    feat: toggle muted in sound button

--- lib/music/widget/mute_button.dart
+++ lib/music/widget/mute_button.dart
@@ -2,15 +2,23 @@
 // https://very-good-ventures-team.monday.com/boards/6004820050/pulses/6417645325
 // coverage:ignore-file
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:io_crossword/settings/settings.dart';
 
 class MuteButton extends StatelessWidget {
   const MuteButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () {},
-      icon: const Icon(Icons.volume_off),
+    final settings = context.watch<SettingsController>();
+    return ValueListenableBuilder<bool>(
+      valueListenable: settings.muted,
+      builder: (_, muted, __) {
+        return IconButton(
+          onPressed: settings.toggleMuted,
+          icon: Icon(muted ? Icons.volume_off : Icons.volume_up),
+        );
+      },
     );
   }
 }
