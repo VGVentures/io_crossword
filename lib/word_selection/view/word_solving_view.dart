02211d74913b2cde1ea6e5290859ff342d852082
Alejandro Santiago <dev@alestiago.com>
2024-05-03 11:49:21 +0100

    chore: remove redundant widgets import

--- lib/word_selection/view/word_solving_view.dart
+++ lib/word_selection/view/word_solving_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_crossword/crossword2/crossword2.dart';
 import 'package:io_crossword/hint/hint.dart';
