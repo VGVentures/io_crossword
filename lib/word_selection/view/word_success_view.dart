cdce1af85e348f6d448fb5dac7ff13e7d4694a70
Alejandro Santiago <dev@alestiago.com>
2024-05-08 15:16:39 +0100

    chore: removed redundant imports

--- lib/word_selection/view/word_success_view.dart
+++ lib/word_selection/view/word_success_view.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_crossword/assets/assets.dart';
 import 'package:io_crossword/audio/audio.dart';
