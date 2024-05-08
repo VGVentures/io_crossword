fc6d984c4aef8f851eb6394697f2f7faa39ba0c7
AyadLaouissi <ayadlaouissi@gmail.com>
2024-05-08 14:26:12 +0200

    feat: creates app_bloc.dart

---
+++ test/app/bloc/app_event_test.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:io_crossword/app/bloc/app_bloc.dart';
+
+void main() {
+  group('$AppEvent', () {
+    group('$UserLoaded', () {
+      test('supports equality', () {
+        expect(UserLoaded(), equals(UserLoaded()));
+      });
+    });
+
+    group('$LogOutUser', () {
+      test('supports equality', () {
+        expect(LogOutUser(), equals(LogOutUser()));
+      });
+    });
+  });
+}
