6a417d65f187e25905e3dd8fc0f2b3c83ebe8c80
Hugo Walbecq <hugo@verygood.ventures>
2024-05-10 10:05:41 +0200

    feat: zoom on word selected (#456)
    * feat: change animation to matrix tween + check if word is scaled
    
    * feat: scale translation vector
    
    * feat: clean calculations
    
    * feat: remove scale enabled
    
    * feat: back to scale of 1 if selected word fits
    
    * test: add scale tests
    
    * refactor: rename centerAnimation to transformationAnimation
    
    * refactor: remove trailing comma
    
    * refactor: removed unecessary variable in wordFitsInViewport
    
    * refactor: rename centerBegin and centerEnd to transformationBegin and transformationEnd
    
    * refactor: change conditional
    
    * refactor: moved translationBegin
    
    * refactor: rename currentScale to scaleBegin
    
    * refactor: rename updatedScale to scaleEnd
    
    * refactor: simplified scaleEnd
    
    * refactor: renamed center to reducedViewportCenter
    
    * refactor: moved word calculations
    
    * feat: changed linear curve
    
    * refactor: changed guards
    
    * refactor: moved layout variable
    
    * chore: remove print
    
    * feat[wip]: pair with Hugo 🍐
    
    * feat: fixing final position
    
    * feat: use beviewport as end
    
    ---------
    
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>

--- test/crossword2/widgets/crossword_interactive_viewer_test.dart
+++ test/crossword2/widgets/crossword_interactive_viewer_test.dart
@@ -7,8 +7,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:game_domain/game_domain.dart' as domain show Axis;
 import 'package:game_domain/game_domain.dart' hide Axis;
 import 'package:io_crossword/crossword2/crossword2.dart';
-import 'package:io_crossword/crossword2/widgets/widgets.dart';
 import 'package:io_crossword/word_selection/word_selection.dart';
+import 'package:io_crossword_ui/io_crossword_ui.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../helpers/helpers.dart';
@@ -65,6 +65,93 @@ void main() {
         expect(QuadScope.of(buildContext), equals(quad));
       },
     );
+
+    group('scales selected word', () {
+      late _MockWord word;
+
+      setUp(() {
+        word = _MockWord();
+        when(() => word.id).thenReturn('id');
+        when(() => word.axis).thenReturn(domain.Axis.horizontal);
+        when(() => word.position).thenReturn(const Point(0, 0));
+        wordSelectionBloc = _MockWordSelectionBloc();
+        when(() => word.solvedCharacters).thenReturn({});
+
+        when(() => wordSelectionBloc.state)
+            .thenReturn(const WordSelectionState.initial());
+        when(() => wordSelectionBloc.state).thenReturn(
+          WordSelectionState(
+            status: WordSelectionStatus.solving,
+            word: SelectedWord(
+              section: (0, 0),
+              word: word,
+            ),
+          ),
+        );
+        when(() => word.isSolved).thenReturn(false);
+      });
+
+      testWidgets(
+        'with normal word',
+        (tester) async {
+          when(() => word.length).thenReturn(5);
+          await tester.pumpApp(
+            layout: IoLayoutData.large,
+            DefaultWordInputController(
+              child: BlocProvider<WordSelectionBloc>(
+                create: (_) => wordSelectionBloc,
+                child: const Crossword2View(),
+              ),
+            ),
+          );
+
+          await tester.ensureVisible(find.byType(EditableText).first);
+          await tester.ensureVisible(find.byType(EditableText).last);
+          expect(find.byType(EditableText), findsNWidgets(5));
+        },
+      );
+
+      testWidgets(
+        'with big horizontal word',
+        (tester) async {
+          when(() => word.length).thenReturn(25);
+          await tester.pumpApp(
+            layout: IoLayoutData.large,
+            DefaultWordInputController(
+              child: BlocProvider<WordSelectionBloc>(
+                create: (_) => wordSelectionBloc,
+                child: const Crossword2View(),
+              ),
+            ),
+          );
+
+          await tester.ensureVisible(find.byType(EditableText).first);
+          await tester.ensureVisible(find.byType(EditableText).last);
+          expect(find.byType(EditableText), findsNWidgets(25));
+        },
+      );
+
+      testWidgets(
+        'with big vertical word',
+        (tester) async {
+          when(() => word.length).thenReturn(25);
+          when(() => word.axis).thenReturn(domain.Axis.vertical);
+          await tester.pumpApp(
+            layout: IoLayoutData.large,
+            DefaultWordInputController(
+              child: BlocProvider<WordSelectionBloc>(
+                create: (_) => wordSelectionBloc,
+                child: const Crossword2View(),
+              ),
+            ),
+          );
+
+          await tester.ensureVisible(find.byType(EditableText).first);
+          await tester.ensureVisible(find.byType(EditableText).last);
+          expect(find.byType(EditableText), findsNWidgets(25));
+        },
+      );
+    });
 
     group('panning', () {
       late _MockWord word;
