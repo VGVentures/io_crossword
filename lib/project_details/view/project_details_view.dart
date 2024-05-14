3362e52d6c6a020051bba075a3e765d5b0d00334
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-05-14 15:59:44 -0700

    feat: adjust project details view

--- lib/project_details/view/project_details_view.dart
+++ lib/project_details/view/project_details_view.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:io_crossword/assets/assets.dart';
-import 'package:io_crossword/extensions/extensions.dart';
 import 'package:io_crossword/l10n/l10n.dart';
-import 'package:io_crossword/project_details/link/project_details_links.dart';
 import 'package:io_crossword/widget/widget.dart';
 import 'package:io_crossword_ui/io_crossword_ui.dart';
 
@@ -60,9 +57,6 @@ class ProjectDetailsContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
-    final textTheme = Theme.of(context).textTheme;
-    const linkColor = IoCrosswordColors.linkBlue;
     final layout = IoLayout.of(context);
     final verticalSpacing = layout == IoLayoutData.small ? 24.0 : 40.0;
 
@@ -91,86 +85,16 @@ class ProjectDetailsContent extends StatelessWidget {
             ),
             SizedBox(height: verticalSpacing),
           ],
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Column(
-                children: [
-                  if (layout == IoLayoutData.large)
-                    const _ProjectDetailsHeader(),
-                  if (layout == IoLayoutData.small)
-                    Assets.images.hero.image(height: 154),
-                  SizedBox(height: verticalSpacing),
-                  const HowMade(),
-                  SizedBox(height: verticalSpacing),
-                  Text(
-                    '${l10n.otherLinks}:',
-                    style: textTheme.bodyLarge?.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  RichText(
-                    text: TextSpan(
-                      text: 'Google I/O',
-                      style: textTheme.bodyLarge?.copyWith(
-                        color: linkColor,
-                        decoration: TextDecoration.underline,
-                        decorationColor: linkColor,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          context.launchUrl(ProjectDetailsLinks.googleIO);
-                        },
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  RichText(
-                    text: TextSpan(
-                      text: l10n.privacyPolicy,
-                      style: textTheme.bodyLarge?.copyWith(
-                        color: linkColor,
-                        decoration: TextDecoration.underline,
-                        decorationColor: linkColor,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          context.launchUrl(ProjectDetailsLinks.privacyPolicy);
-                        },
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  RichText(
-                    text: TextSpan(
-                      text: l10n.termsOfService,
-                      style: textTheme.bodyLarge?.copyWith(
-                        color: linkColor,
-                        decoration: TextDecoration.underline,
-                        decorationColor: linkColor,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          context.launchUrl(ProjectDetailsLinks.termsOfService);
-                        },
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  RichText(
-                    text: TextSpan(
-                      text: l10n.faqs,
-                      style: textTheme.bodyLarge?.copyWith(
-                        color: linkColor,
-                        decoration: TextDecoration.underline,
-                        decorationColor: linkColor,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          context.launchUrl(ProjectDetailsLinks.faqs);
-                        },
-                    ),
-                  ),
-                ],
-              ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                if (layout == IoLayoutData.large) const _ProjectDetailsHeader(),
+                if (layout == IoLayoutData.small)
+                  Assets.images.hero.image(height: 200),
+                const SizedBox(height: 60),
+                const HowMade(),
+              ],
             ),
           ),
         ],
