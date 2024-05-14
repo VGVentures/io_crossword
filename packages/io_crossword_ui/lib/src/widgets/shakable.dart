ba385bedb025051bffbf8bd53fd5176e4ffb8c1a
Alejandro Santiago <dev@alestiago.com>
2024-05-13 17:06:27 -0700

    feat: avoid resetting CrosswordInput when invalid

--- packages/io_crossword_ui/lib/src/widgets/shakable.dart
+++ packages/io_crossword_ui/lib/src/widgets/shakable.dart
@@ -6,82 +6,38 @@ part of 'io_word_input.dart';
 class Shakable extends StatefulWidget {
   /// {@macro shakable}
   const Shakable({
+    required this.controller,
     required this.child,
-    required this.shakeDuration,
-    this.controller,
     this.speed = 6,
-    @visibleForTesting this.animationController,
     super.key,
   });
 
-  /// {@macro io_word_input_controller}
-  final IoWordInputController? controller;
+  /// Controls the shaking animation.
+  final AnimationController controller;
 
-  /// Animation controller.
-  ///
-  /// Usable only for testing.
-  @visibleForTesting
-  final AnimationController? animationController;
-
-  /// Child widget.
+  /// The widget to shake.
   final Widget child;
-
-  /// Duration of the shaking animation.
-  final Duration shakeDuration;
 
   /// Speed of the animation.
   final int speed;
 
   @override
-  State<Shakable> createState() => ShakableState();
+  State<Shakable> createState() => _ShakableState();
 }
 
 /// {@template shakable_state}
 /// State class of the [Shakable] animation.
 /// {@endtemplate}
-@visibleForTesting
-class ShakableState extends State<Shakable>
+class _ShakableState extends State<Shakable>
     with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController =
-      widget.animationController ??
-          AnimationController(
-            vsync: this,
-            duration: widget.shakeDuration,
-          );
-
-  /// Starts the shaking animation and resets it when it ends if word is empty.
-  void _onInputReset() {
-    final controller = widget.controller;
-
-    if (controller != null && controller.didReset) {
-      _animationController.forward(from: 0).then(
-            (value) => _animationController.reset(),
-          );
-    }
-  }
-
-  @override
-  void dispose() {
-    widget.controller?.removeListener(_onInputReset);
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    widget.controller?.addListener(_onInputReset);
-  }
-
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _animationController,
+      animation: widget.controller,
       child: widget.child,
       builder: (context, child) {
         final sinValue = sin(
-          widget.speed * pi * _animationController.value,
+          widget.speed * pi * widget.controller.value,
         );
         return Transform.translate(
           offset: Offset(sinValue * 10, 0),
