8839e589509e01ec22c1c565f27771507173e3ec
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-05-21 15:40:38 +0200

    chore: update web initialization logic

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,43 @@
+/// Keep in mind that this file is not a real javascript file, it is a template
+/// that will be processed by the build system to generate the final javascript
+/// file. Therefore, we use the mustache syntax, like {{variable}}, to inject
+/// values and you might see some linter errors in this file.
+
+{{flutter_js}}
+{{flutter_build_config}}
+
+const cookieBar = document.querySelector("#cookie-bar");
+const cookieBarButton = document.querySelector("#confirm-cookies");
+const splashCopy = document.querySelector("#splash_copy");
+
+const additionalScripts = [];
+
+let cookiesAcceptanceCompleter;
+let hasUserAcceptedCookies = new Promise((resolve) => {
+  cookiesAcceptanceCompleter = resolve;
+});
+
+cookieBarButton.addEventListener("click", () => {
+  console.log("cookieBarButton clicked");
+  cookieBar.remove();
+  cookiesAcceptanceCompleter();
+});
+
+window.addEventListener("load", (event) => {
+  _flutter.loader.load({
+    serviceWorkerSettings: {
+      serviceWorkerVersion: {{flutter_service_worker_version}},
+    },
+    onEntrypointLoaded: async function (engineInitializer) {
+      const appRunner = await engineInitializer.initializeEngine();
+      await hasUserAcceptedCookies;
+
+      window.addEventListener("flutter-first-frame", function () {
+        splashCopy.remove();
+        document.body.classList.remove("loading-mode");
+      });
+
+      await appRunner.runApp();
+    },
+  });
+});
