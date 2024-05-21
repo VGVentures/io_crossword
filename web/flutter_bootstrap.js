da011e068291bbfe7b4b304d7c34b996730e0e3a
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-05-21 18:54:44 +0200

    docs: format correctly

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,4 +1,5 @@
-/** Keep in mind that this file is not a real javascript file, it is a template
+/**
+ * Keep in mind that this file is not a real javascript file, it is a template
  * that will be processed by the build system to generate the final javascript
  * file. Therefore, we use the {@link https://mustache.github.io/mustache.5.html mustache syntax} to inject
  * values and you might see some linter errors in this file.
