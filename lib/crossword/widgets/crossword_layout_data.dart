cf300846bb7089c4f929cfe4a956f2a5ffa999eb
Jaime Sanchez <jaimesanchez8@gmail.com>
2024-05-27 18:00:06 +0200

    chore: remove unused import

--- lib/crossword/widgets/crossword_layout_data.dart
+++ lib/crossword/widgets/crossword_layout_data.dart
@@ -1,5 +1,4 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:io_crossword/crossword/crossword.dart';
 
