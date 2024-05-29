f27e14fda6a0dedb63313f1124b2792b448d6194
Jaime <52668514+jsgalarraga@users.noreply.github.com>
2024-05-29 18:15:05 +0200

    fix: make share url dependent on environment (#540)

--- lib/project_details/link/project_details_links.dart
+++ lib/project_details/link/project_details_links.dart
@@ -7,6 +7,9 @@ class ProjectDetailsLinks {
   static const claimBadge =
       'https://developers.google.com/profile/badges/events/io/2024/crossword';
   static const googleAI = 'https://ai.google.dev/';
-  static const crossword = 'https://e7b2896c65fabc0480edc6c0e.web.app';
+  static const crossword = String.fromEnvironment(
+    'CROSSWORD_URL',
+    defaultValue: 'https://crossword.withgoogle.com',
+  );
   static const blogPost = 'https://flutter.dev/crossword';
 }
